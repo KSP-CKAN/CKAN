e1d2e1180bf6d19065f3e368b45f0a9d82877b2f
Paul Fenwick <pjf@perltraining.com.au>
2014-09-24 18:27:40 +1000

    Added basic directory walking to find modules (CKAN/DLL).

--- CKAN/CKAN/ModuleDict.cs
+++ CKAN/CKAN/ModuleDict.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 // Find all the modules we have installed.
 // This may be from a local cache, or by crawling GameData itself.
@@ -18,10 +19,43 @@ namespace CKAN {
 			// TODO: It would be great to optimise this to skip .git directories and the like.
 			// Yes, I keep my GameData in git.
 
-			string[] files = Directory.GetFiles(gameData, "*.ckan", SearchOption.AllDirectories);
+			// TODO: Optimisation: Do all the below in a single pass, rather than walking
+			// GameData twice.
 
-			foreach (string file in files) {
+			// Find all the CKAN files. Presumably, these are actually representative
+			// of what we have installed.
+
+			Console.WriteLine ("Finding CKAN files");
+
+			string[] ckanFiles = Directory.GetFiles(gameData, "*.ckan", SearchOption.AllDirectories);
+
+			foreach (string file in ckanFiles) {
 				Console.WriteLine (file);
+			}
+
+			// Find all the DLLs. ModuleManager assumes that if a DLL exists, then a mod
+			// is installed by the same name (after clipping off versions).
+
+			Console.WriteLine ("Finding DLLs");
+
+			string[] dllFiles = Directory.GetFiles (gameData, "*.dll", SearchOption.AllDirectories);
+
+			// TODO: Check how ModuleManager transforms DLL names into mod names, and make
+			// sure we do things the same way.
+
+			foreach (string file in dllFiles) {
+
+				// We're going to clip at the first period to get the ModuleName.
+				// We'll get some false positives from bundled support libraries, but nobody's
+				// going to depend upon those or treat them as proper mods (I hope!)
+
+				string module = Regex.Replace (file, "^.*/([^.]+).*", "$1");
+
+				this [module] = new Module ();
+				this [module]._version = "0";     // We can say it exists, but have no idea of other info.
+				this [module]._identifier = module;
+
+				Console.WriteLine (this[module]._identifier + " ( " + file + " ) ");
 			}
 		}
 	}
