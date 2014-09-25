eb282c91ca95df19616b1a3f8df8f906bac4cef2
Paul Fenwick <pjf@perltraining.com.au>
2014-09-25 15:29:13 +1000

    Retired ModuleDict. Moved scanGameData to KSP.

--- CKAN/CKAN/KSP.cs
+++ CKAN/CKAN/KSP.cs
@@ -14,5 +14,28 @@ namespace CKAN {
 				".steam", "steam", "SteamApps", "common", "Kerbal Space Program", "GameData"
 			);
 		}
+
+		public static void scanGameData() {
+
+			// TODO: Get rid of magic paths!
+			RegistryManager registry_manager = new RegistryManager("/tmp/ksp_registry");
+
+			Registry registry = registry_manager.load_or_create ();
+
+			// Forget that we've seen any DLLs, as we're going to refresh them all.
+			registry.clear_dlls ();
+
+			// TODO: It would be great to optimise this to skip .git directories and the like.
+			// Yes, I keep my GameData in git.
+
+			string[] dllFiles = Directory.GetFiles (gameData(), "*.dll", SearchOption.AllDirectories);
+
+			foreach (string file in dllFiles) {
+				// register_dll does the heavy lifting of turning it into a modname
+				registry.register_dll (file);
+			}
+
+			registry_manager.save (registry);
+		}
 	}
 }
