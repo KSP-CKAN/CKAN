e42a48496394bffc92a615b395cfc2192fb51b0f
Paul Fenwick <pjf@perltraining.com.au>
2014-10-04 05:16:04 +1000

    Layout changes only. Tabs -> 4 spaces

--- Registry.cs
+++ Registry.cs
@@ -4,99 +4,99 @@ using System.Text.RegularExpressions;
 
 namespace CKAN
 {
-	class RegistryVersionNotSupportedException : Exception
-	{
-		public int requested_version;
+    class RegistryVersionNotSupportedException : Exception
+    {
+        public int requested_version;
 
-		public RegistryVersionNotSupportedException (int v)
-		{
-			requested_version = v;
-		}
-	}
+        public RegistryVersionNotSupportedException (int v)
+        {
+            requested_version = v;
+        }
+    }
 
-	public class Registry
-	{
-		const int LATEST_REGISTRY_VERSION = 0;
-		public int registry_version;
-		public Dictionary<string, InstalledModule> installed_modules;
-		public Dictionary<string, string> installed_dlls;
+    public class Registry
+    {
+        const int LATEST_REGISTRY_VERSION = 0;
+        public int registry_version;
+        public Dictionary<string, InstalledModule> installed_modules;
+        public Dictionary<string, string> installed_dlls;
 
-		public Registry (int version, Dictionary<string, InstalledModule> mods, Dictionary<string, string> dlls)
-		{
-			/* TODO: support more than just the latest version */
-			if (version != LATEST_REGISTRY_VERSION) {
-				throw new RegistryVersionNotSupportedException (version);
-			}
+        public Registry (int version, Dictionary<string, InstalledModule> mods, Dictionary<string, string> dlls)
+        {
+            /* TODO: support more than just the latest version */
+            if (version != LATEST_REGISTRY_VERSION) {
+                throw new RegistryVersionNotSupportedException (version);
+            }
 
-			installed_modules = mods;
-			installed_dlls    = dlls;
-		}
+            installed_modules = mods;
+            installed_dlls    = dlls;
+        }
 
-		public static Registry empty ()
-		{
-			return new Registry (LATEST_REGISTRY_VERSION, new Dictionary<string, InstalledModule> (), new Dictionary<string,string> () );
-		}
+        public static Registry empty ()
+        {
+            return new Registry (LATEST_REGISTRY_VERSION, new Dictionary<string, InstalledModule> (), new Dictionary<string,string> () );
+        }
 
-		/// <summary>
-		/// Register the supplied module as having been installed, thereby keeping
-		/// track of its metadata and files.
-		/// </summary>
-		/// <param name="mod">Mod.</param>
-		public void register_module (InstalledModule mod)
-		{
-			installed_modules.Add (mod.source_module.identifier, mod);
-		}
+        /// <summary>
+        /// Register the supplied module as having been installed, thereby keeping
+        /// track of its metadata and files.
+        /// </summary>
+        /// <param name="mod">Mod.</param>
+        public void register_module (InstalledModule mod)
+        {
+            installed_modules.Add (mod.source_module.identifier, mod);
+        }
 
-		public void deregister_module(string module) {
-			installed_modules.Remove (module);
-		}
+        public void deregister_module(string module) {
+            installed_modules.Remove (module);
+        }
 
-		public void register_dll (string path)
-		{
-			// Oh my, does .NET support extended regexps (like Perl?), we could use them right now.
-			Match match = Regex.Match (path, @".*?(?:^|/)GameData/((?:.*/|)([^.]+).*dll)");
+        public void register_dll (string path)
+        {
+            // Oh my, does .NET support extended regexps (like Perl?), we could use them right now.
+            Match match = Regex.Match (path, @".*?(?:^|/)GameData/((?:.*/|)([^.]+).*dll)");
 
-			string relPath = match.Groups[1].Value;
-			string modName = match.Groups[2].Value;
+            string relPath = match.Groups[1].Value;
+            string modName = match.Groups[2].Value;
 
-			Console.WriteLine ("Registering {0} -> {1}", modName, relPath);
+            Console.WriteLine ("Registering {0} -> {1}", modName, relPath);
 
-			// We're fine if we overwrite an existing key.
-			installed_dlls[modName] = relPath;
-		}
+            // We're fine if we overwrite an existing key.
+            installed_dlls[modName] = relPath;
+        }
 
-		public void clear_dlls() {
-			installed_dlls = new Dictionary<string,string> ();
-		}
+        public void clear_dlls() {
+            installed_dlls = new Dictionary<string,string> ();
+        }
 
-		/// <summary>
-		/// Returns the installed version of a given mod.
-		/// If the mod was autodetected (but present), a "0" is returned.
-		/// If the mod is not found, a null will be returned.
-		/// </summary>
-		/// <returns>The version.</returns>
-		/// <param name="modName">Mod name.</param>
+        /// <summary>
+        /// Returns the installed version of a given mod.
+        /// If the mod was autodetected (but present), a "0" is returned.
+        /// If the mod is not found, a null will be returned.
+        /// </summary>
+        /// <returns>The version.</returns>
+        /// <param name="modName">Mod name.</param>
 
-		public string installedVersion(string modName) {
-			if (installed_modules.ContainsKey(modName)) {
-				return installed_modules [modName].source_module.version;
-			}
-			else if (installed_dlls.ContainsKey(modName)) {
-				return "0";	// We probably want a better way to signal auto-detected modules.
-			}
+        public string installedVersion(string modName) {
+            if (installed_modules.ContainsKey(modName)) {
+                return installed_modules [modName].source_module.version;
+            }
+            else if (installed_dlls.ContainsKey(modName)) {
+                return "0";    // We probably want a better way to signal auto-detected modules.
+            }
 
-			return null;
-		}
+            return null;
+        }
 
-		/// <summary>
-		/// Check if a mod is installed (either via CKAN, or a DLL detected)
-		/// </summary>
-		/// <returns><c>true</c>, if installed<c>false</c> otherwise.</returns>
-		public bool isInstalled(string modName) {
-			if (installedVersion (modName) == null) {
-				return false;
-			}
-			return true;
-		}
-	}
+        /// <summary>
+        /// Check if a mod is installed (either via CKAN, or a DLL detected)
+        /// </summary>
+        /// <returns><c>true</c>, if installed<c>false</c> otherwise.</returns>
+        public bool isInstalled(string modName) {
+            if (installedVersion (modName) == null) {
+                return false;
+            }
+            return true;
+        }
+    }
 }
--- RegistryManager.cs
+++ RegistryManager.cs
@@ -5,68 +5,68 @@ using System.Collections.Generic;
 
 namespace CKAN
 {
-	public class RegistryManager
-	{
-		string path;
-		public Registry registry; 
-		static Dictionary<string, RegistryManager> singleton = new Dictionary<string, RegistryManager> ();
+    public class RegistryManager
+    {
+        string path;
+        public Registry registry; 
+        static Dictionary<string, RegistryManager> singleton = new Dictionary<string, RegistryManager> ();
 
-		public static RegistryManager Instance (string path = null) {
+        public static RegistryManager Instance (string path = null) {
 
-			if (path == null) {
-				path = defaultRegistry ();
-			}
+            if (path == null) {
+                path = defaultRegistry ();
+            }
 
-			// If we've already got a registry for this, then return it.
-			if (! singleton.ContainsKey (path)) {
-				singleton [path] = new RegistryManager (path);
-			}
+            // If we've already got a registry for this, then return it.
+            if (! singleton.ContainsKey (path)) {
+                singleton [path] = new RegistryManager (path);
+            }
 
-			return singleton [path];
-		}
+            return singleton [path];
+        }
 
-		// We require our constructor to be private so we can
-		// enforce this being an instance.
-		private RegistryManager (string path)
-		{
+        // We require our constructor to be private so we can
+        // enforce this being an instance.
+        private RegistryManager (string path)
+        {
 
-			this.path = path;
-			this.load_or_create ();
+            this.path = path;
+            this.load_or_create ();
 
-			singleton [path] = this;
-		}
+            singleton [path] = this;
+        }
 
-		// Default registry location
-		private static string defaultRegistry() {
-			return Path.Combine (KSP.ckanDir(), "registry.json");
-		}
+        // Default registry location
+        private static string defaultRegistry() {
+            return Path.Combine (KSP.ckanDir(), "registry.json");
+        }
 
-		public void load() {
-			string json = System.IO.File.ReadAllText(path);
-			registry = JsonConvert.DeserializeObject<Registry>(json);
-		}
+        public void load() {
+            string json = System.IO.File.ReadAllText(path);
+            registry = JsonConvert.DeserializeObject<Registry>(json);
+        }
 
-		public void load_or_create() {
-			try {
-				load ();
-			}
-			catch (System.IO.FileNotFoundException) {
-				create ();
-				load ();
-			}
-		}
+        public void load_or_create() {
+            try {
+                load ();
+            }
+            catch (System.IO.FileNotFoundException) {
+                create ();
+                load ();
+            }
+        }
 
-		void create() {
-			registry = Registry.empty ();
-			save ();
-		}
+        void create() {
+            registry = Registry.empty ();
+            save ();
+        }
 
-		public string serialise () {
-			return JsonConvert.SerializeObject (registry);
-		}
+        public string serialise () {
+            return JsonConvert.SerializeObject (registry);
+        }
 
-		public void save () {
-			System.IO.File.WriteAllText(path, serialise ());
-		}
-	}
+        public void save () {
+            System.IO.File.WriteAllText(path, serialise ());
+        }
+    }
 }
