f1be0105d66235418be974f1edadd73e9fea170d
Paul Fenwick <pjf@perltraining.com.au>
2014-10-04 19:36:54 +1000

    Use `git describe --long` for version numbers.

--- CKAN/CKAN/Properties/AssemblyInfo.cs
+++ CKAN/CKAN/Properties/AssemblyInfo.cs
@@ -14,7 +14,11 @@ using System.Runtime.CompilerServices;
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion ("0.0.1.*")]
+
+// We don't use AssemblyVersions, since the build system adds them for us,
+// based upon git tags and commit numbers.
+//[assembly: AssemblyVersion ("0.0.1.*")]
+
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
