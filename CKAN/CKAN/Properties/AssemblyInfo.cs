47d9b6c51b2599b2d5250a120fababd622cba4d0
Paul Fenwick <pjf@perltraining.com.au>
2014-10-04 18:49:28 +1000

    Add `ckan version`, with support from the build process.

--- CKAN/CKAN/Properties/AssemblyInfo.cs
+++ CKAN/CKAN/Properties/AssemblyInfo.cs
@@ -4,19 +4,20 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 [assembly: AssemblyTitle ("CKAN")]
-[assembly: AssemblyDescription ("")]
+[assembly: AssemblyDescription ("The Comprehensive Kerbal Archive Network")]
 [assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("")]
+[assembly: AssemblyCompany ("https://github.com/pjf/CKAN")]
 [assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("pjf")]
+[assembly: AssemblyCopyright ("CC-BY 4.0, LGPL, or MIT; you choose!")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion ("1.0.*")]
+[assembly: AssemblyVersion ("0.0.1.*")]
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
 
+// NB: The CKAN build/release process appends an AssemblyInformationalVersion to this file.
