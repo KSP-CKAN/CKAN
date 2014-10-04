605e008cec020a5fc886830ab3f3208b2618cd3b
Paul Fenwick <pjf@perltraining.com.au>
2014-10-04 13:13:02 +1000

    Added registry-aware KSP.SteamPath(), updated KSP.gameDir().
    Part of the work on GH #23.

--- CKAN/CKAN/KSP.cs
+++ CKAN/CKAN/KSP.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using Microsoft.Win32;
 
 /// <summary>
 /// Everything for dealing with KSP itself.
@@ -8,14 +9,60 @@ using System.IO;
 namespace CKAN {
     public class KSP {
 
+        // Where to find KSP relative to Steam's root.
+        // TODO: How do we make this variable immutable?
+        static string steamKSP = Path.Combine( "SteamApps", "common", "Kerbal Space Program" );
+
+        /// <summary>
+        /// Finds Steam on the current machine.
+        /// </summary>
+        /// <returns>The path to steam, or null if not found</returns>
+        static string SteamPath() {
+            // First check the registry.
+
+            string steam = (string) Microsoft.Win32.Registry.GetValue (@"HKEY_CURRENT_USER\Software\Valve\SteamPath", "", null);
+
+            // If that directory exists, we've found steam!
+            if (steam != null && FileSystem.IsDirectory(steam)) {
+                return steam;
+            }
+
+            // Not in the registry, or missing file, but that's cool. This should find it on Linux/OSX
+
+            steam = Path.Combine (
+                Environment.GetFolderPath (Environment.SpecialFolder.Personal),
+                ".steam", "steam"
+            );
+
+            if (FileSystem.IsDirectory (steam)) {
+                return steam;
+            }
+
+            // Nope, can't find steam.
+            return null;
+        }
+
         public static string gameDir() {
 
-            // TODO: Have this *actually* find our GameData directory!
+            // TODO: Cache the result of this.
 
-            return Path.Combine (
-                Environment.GetFolderPath (Environment.SpecialFolder.Personal),
-                ".steam", "steam", "SteamApps", "common", "Kerbal Space Program"
-            );
+            // TODO: See if KSP was specified on the command line.
+
+            // TODO: See if KSP is in the same dir as we're installed (GH #23)
+
+            // TODO: See if we've got it cached in the registry.
+
+            // See if we can find KSP as part of a Steam install.
+
+            string steam = SteamPath ();
+            if (steam != null) {
+                return Path.Combine (steam, steamKSP);
+            }
+
+            // Oh noes! We can't find KSP!
+
+            throw new DirectoryNotFoundException ();
+
         }
     
         public static string gameData() {
