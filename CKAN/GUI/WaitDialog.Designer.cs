50f9fe0ff0162ceff4278df33d6f1ac309b8663b
Alexander Dzhoganov <alexander.dzhoganov@gmail.com>
2014-10-16 17:05:12 +0300

    added progress monitoring for installs

--- CKAN/GUI/WaitDialog.Designer.cs
+++ CKAN/GUI/WaitDialog.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.ModalLabel = new System.Windows.Forms.Label();
+            this.DescriptionLabel = new System.Windows.Forms.Label();
+            this.ActionDescriptionLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // ModalLabel
@@ -40,11 +42,34 @@
             this.ModalLabel.TabIndex = 0;
             this.ModalLabel.Text = "Waiting for operation to complete";
             // 
+            // DescriptionLabel
+            // 
+            this.DescriptionLabel.AutoSize = true;
+            this.DescriptionLabel.Location = new System.Drawing.Point(61, 56);
+            this.DescriptionLabel.Name = "DescriptionLabel";
+            this.DescriptionLabel.Size = new System.Drawing.Size(0, 13);
+            this.DescriptionLabel.TabIndex = 1;
+            // 
+            // ActionDescriptionLabel
+            // 
+            this.ActionDescriptionLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ActionDescriptionLabel.AutoSize = true;
+            this.ActionDescriptionLabel.Location = new System.Drawing.Point(61, 56);
+            this.ActionDescriptionLabel.Name = "ActionDescriptionLabel";
+            this.ActionDescriptionLabel.Size = new System.Drawing.Size(19, 13);
+            this.ActionDescriptionLabel.TabIndex = 2;
+            this.ActionDescriptionLabel.Text = "(..)";
+            this.ActionDescriptionLabel.Click += new System.EventHandler(this.ActionDescriptionLabel_Click);
+            // 
             // WaitDialog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(292, 89);
+            this.Controls.Add(this.ActionDescriptionLabel);
+            this.Controls.Add(this.DescriptionLabel);
             this.Controls.Add(this.ModalLabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "WaitDialog";
@@ -57,5 +82,7 @@
         #endregion
 
         private System.Windows.Forms.Label ModalLabel;
+        private System.Windows.Forms.Label DescriptionLabel;
+        private System.Windows.Forms.Label ActionDescriptionLabel;
     }
 }
