9262570868678b17b90a79173f93286cea85974c
Paul Fenwick <pjf@perltraining.com.au>
2014-10-17 11:47:39 +1100

    Remove dependency on `System.Manangement.*`, for #80.

--- Main.cs
+++ Main.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Management.Instrumentation;
 using System.Reflection;
 using System.Windows.Forms;
 
