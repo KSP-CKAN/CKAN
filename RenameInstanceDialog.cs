2be841b2c079d30002cdac374372f958c511bed3
Alexander Dzhoganov <alexander.dzhoganov@gmail.com>
2014-10-21 15:17:03 +0300

    Added rename instance window

---
+++ RenameInstanceDialog.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CKAN
+{
+    public partial class RenameInstanceDialog : Form
+    {
+        public RenameInstanceDialog()
+        {
+            InitializeComponent();
+        }
+
+        public DialogResult ShowRenameInstanceDialog(string name)
+        {
+            NameTextBox.Text = name;
+            return ShowDialog();
+        }
+
+        public string GetResult()
+        {
+            return NameTextBox.Text;
+        }
+
+        private void RenameInstanceDialog_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
