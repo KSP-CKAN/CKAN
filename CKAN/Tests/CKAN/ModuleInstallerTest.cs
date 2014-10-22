db84d7aacf27a3b0728913a1588db242a18b00dd
Paul Fenwick <pjf@perltraining.com.au>
2014-10-22 15:20:36 +1100

    Added GenerateDefaultInstall().
    For #127.

---
+++ CKAN/Tests/CKAN/ModuleInstallerTest.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+using System;
+
+namespace Tests
+{
+    [TestFixture()]
+    public class ModuleInstallerTest
+    {
+        // [Test()]
+        // TODO: Test me!
+        public void GenerateDefaultInstall()
+        {
+        }
+    }
+}
+
