4dced9e82f099a1871ceb8a4e427cc98383d08e4
Paul Fenwick <pjf@perltraining.com.au>
2014-10-25 02:01:17 +1100

    Exclude flaky network tests from being run by travis.
    These can ruin our day if they fail bogusly.

--- CKAN/Tests/KerbalStuff/GitHubTests.cs
+++ CKAN/Tests/KerbalStuff/GitHubTests.cs
@@ -7,7 +7,13 @@ namespace NetKAN.GitHubTests
     [TestFixture()]
     public class GithubAPITests
     {
+
+        // Ironically, despite the fact that these run on travis-ci, which is strongly integrated
+        // to github, these sometimes cause test failures because github will throw random
+        // 403s. (Hence we disable them in travis with --exclude=FlakyNetwork)
+
         [Test()]
+        [Category("FlakyNetwork")]
         public void Release ()
         {
             GithubRelease ckan = CKAN.KerbalStuff.GithubAPI.GetLatestRelease("KSP-CKAN/Test");
@@ -18,6 +24,7 @@ namespace NetKAN.GitHubTests
         }
 
         [Test()]
+        [Category("FlakyNetwork")]
         public void TestGithubRelease()
         {
             Assert.AreEqual(GithubRelease.GithubPage("KSP-CKAN/CKAN"), "https://github.com/KSP-CKAN/CKAN");
