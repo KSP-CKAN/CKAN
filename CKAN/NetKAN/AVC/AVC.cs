94fa4b2a3bc2b5e8486e7d7f4142352254ae98f5
Paul Fenwick <pjf@perltraining.com.au>
2014-11-03 15:54:20 +1100

    Added AVC.FromZipFile

--- CKAN/NetKAN/AVC/AVC.cs
+++ CKAN/NetKAN/AVC/AVC.cs
@@ -1,4 +1,9 @@
 using Newtonsoft.Json;
+using ICSharpCode.SharpZipLib.Zip;
+using CKAN;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
 
 namespace CKAN.NetKAN
 {
@@ -17,6 +22,50 @@ namespace CKAN.NetKAN
         [JsonConverter(typeof (JsonAvcToKspVersion))]
         public KSPVersion ksp_version_max;
 
+        /// <summary>
+        /// Locates a version file in the zipfile specified, and returns an AVC object.
+        /// This requires a module object as we *only* search files we might install.
+        /// </summary>
+        public AVC FromZipfile(CkanModule module, string filename)
+        {
+            using (ZipFile zipfile = new ZipFile(filename))
+            {
+                return FromZipFile(module, zipfile);
+            }
+        }
+
+        /// <summary>
+        /// Locates a version file in the zipfile specified, and returns an AVC object.
+        /// This requires a module object as we *only* search files we might install.
+        /// 
+        /// Returns null if no version is found.
+        /// Throws a Kraken if too many versions are found.
+        /// </summary>
+        public AVC FromZipFile(CkanModule module, ZipFile zipfile)
+        {
+            // Get all our version files.
+            List<InstallableFile> files = ModuleInstaller.FindInstallableFiles(module, zipfile, null)
+                .Where(x => x.source.Name.EndsWith(".version")).ToList();
+
+            if (files.Count == 0)
+            {
+                return null;
+            }
+
+            if (files.Count > 1)
+            {
+                throw new Kraken(
+                    string.Format("Too may .version files located: {0}",
+                              string.Join(", ",files.Select(x => x.source.Name))));
+            }
+
+            // Hooray, found our entry. Extract and return it.
+            using (var zipstream = zipfile.GetInputStream(files[0].source))
+            using (StreamReader stream = new StreamReader(zipstream))
+            {
+                return JsonConvert.DeserializeObject<AVC>(stream.ReadToEnd());
+            }
+        }
     }
 }
 
