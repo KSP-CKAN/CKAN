ad41fef49d9853a56a5a13c31f0ffac10e979403
Alexander Dzhoganov <alexander.dzhoganov@gmail.com>
2014-11-04 12:16:47 +0200

    hide console window when in GUI mode (win32 only)

--- Util.cs
+++ Util.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace CKAN
 {
@@ -59,6 +55,22 @@ namespace CKAN
             {
                 // we're on the UI thread, execute directly
                 action();
+            }
+        }
+
+        // hides the console window on windows
+        // useful when running the GUI
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        public static void HideConsoleWindow()
+        {
+            if (!IsLinux)
+            {
+                ShowWindow(GetConsoleWindow(), 0);
             }
         }
 
