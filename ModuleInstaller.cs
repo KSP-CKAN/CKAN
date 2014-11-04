01bd7e121db0cf7311c1979c7472c26572b4bec4
Alexander Dzhoganov <alexander.dzhoganov@gmail.com>
2014-11-04 13:02:29 +0200

    removed unused headers

--- ModuleInstaller.cs
+++ ModuleInstaller.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.IO;
 using System.Linq;
-using System.Runtime.Remoting.Messaging;
 using System.Security.Cryptography;
 using System.Text.RegularExpressions;
 using System.Threading;
