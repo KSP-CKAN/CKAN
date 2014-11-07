92251797de7c3b3bf577b0dc11a238f9a8047414
Alexander Dzhoganov <alexander.dzhoganov@gmail.com>
2014-11-07 12:07:32 +0200

    Added project file

---
+++ CKAN/LocalRepo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalRepo
+{
+    class Program
+    {
+    
+        static void Main(string[] args)
+        {
+            
+        }
+    
+    }
+
+}
