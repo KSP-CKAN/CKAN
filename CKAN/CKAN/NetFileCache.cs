7952367820d5b0549b9cb75ed72329126a4e77f8
Alexander Dzhoganov <alexander.dzhoganov@gmail.com>
2014-11-08 11:09:19 +0200

    documented netfilecache

--- CKAN/CKAN/NetFileCache.cs
+++ CKAN/CKAN/NetFileCache.cs
@@ -6,12 +6,26 @@ using System.Text;
 namespace CKAN
 {
 
+
+    /*
+     * This class allows us to cache downloads by URL
+     * It works using two directories - one to store downloads in-progress, and one to commit finished downloads
+     * URLs are cached by hashing them by taking the first 8 chars of the url's SHA1 hash.
+     * 
+     * To use this class the user would have to:
+     * - Obtain a temporary download path by calling GetTemporaryPathForURL(url)
+     * - Initiate his download in this temporary path
+     * - Call CommitDownload(url, desired_filename) which will move the temporary file to the final location
+     * - The final file will be named such as <hash>-<filename>.zip
+     * - The user can call IsCached(url) to check if a particular url exists in the cache
+     * and GetCachedFilename() to get its filename
+     */
     public class NetFileCache
     {
 
         private string tempPath = "";
         private string cachePath = "";
-    
+   
         public NetFileCache(string _cachePath, string _tempPath = null)
         {
             if (_tempPath == null)
@@ -48,6 +62,25 @@ namespace CKAN
             return cachePath;
         }
 
+        // returns true if a url is already in the cache
+        public bool IsCached(Uri url)
+        {
+            var hash = CreateURLHash(url);
+
+            foreach (var file in Directory.GetFiles(cachePath))
+            {
+                var filename = Path.GetFileName(file);
+                if (filename.StartsWith(hash))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        // returns true if a url is already in the cache
+        // returns the filename in the outFilename parameter
         public bool IsCached(Uri url, out string outFilename)
         {
             var hash = CreateURLHash(url);
@@ -66,12 +99,31 @@ namespace CKAN
             return false;
         }
 
+        // returns the filename of an already cached url or null otherwise
+        public string GetCachedFilename(Uri url)
+        {
+            var hash = CreateURLHash(url);
+
+            foreach (var file in Directory.GetFiles(cachePath))
+            {
+                var filename = Path.GetFileName(file);
+                if (filename.StartsWith(hash))
+                {
+                    return file;
+                }
+            }
+
+            return null;
+        }
+
+        // returns a temporary file for a url to which the user can download
         public string GetTemporaryPathForURL(Uri url)
         {
             var hash = CreateURLHash(url);
             return Path.Combine(tempPath, hash);
         }
 
+        // moves a finished download to the complete downloads directory with a desired filename
         public string CommitDownload(Uri url, string filename)
         {
             var sourcePath = GetTemporaryPathForURL(url);
@@ -84,6 +136,7 @@ namespace CKAN
             return targetPath;
         }
 
+        // returns the 8-byte hash for a given url
         public static string CreateURLHash(Uri url)
         {
             using (var sha1 = new SHA1Managed())
