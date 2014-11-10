f797cf435b66a41ef7775984ca81683cce8e346a
Paul Fenwick <pjf@perltraining.com.au>
2014-11-11 01:07:45 +1100

    Added Meta.ReleaseNumber
    This gives us just the release, such as `v0.23` or `v1.2.4`.
    
    Includes tests.

---
+++ CKAN/Tests/CKAN/Meta.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System.Text.RegularExpressions;
+
+namespace CKANTests
+{
+    [TestFixture]
+    public class Meta
+    {
+        [Test]
+        public void Version()
+        {
+            string version = CKAN.Meta.Version();
+
+            if (version == CKAN.Meta.Development)
+            {
+                Assert.Inconclusive("No version detected - development build");
+            }
+
+            // Let's check there's a commits-since-last-tag and git ID.
+
+            Assert.IsTrue(Regex.IsMatch(version, @"\d+-g[0-9a-f]{7}$"), version);
+        }
+
+        [Test]
+        public void ReleaseNumber()
+        {
+            CKAN.Version version = CKAN.Meta.ReleaseNumber();
+
+            if (version == null)
+            {
+                Assert.Inconclusive("No version detected - developmet build");
+            }
+
+            // We should always be in the form v0.xx (pre-release series),
+            // or vx.x.x (released)
+
+            Assert.IsTrue(
+                Regex.IsMatch(version.ToString(), @"^v(?:0.\d+|\d+\.\d+\.\d+)$"),
+                version.ToString());
+        }
+    }
+}
+
