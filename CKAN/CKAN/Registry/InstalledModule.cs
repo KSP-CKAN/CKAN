d4c155b2e13d4ff270b756872e99a5753ed9d435
Paul Fenwick <pjf@perltraining.com.au>
2014-11-12 02:13:01 +1100

    Newly installed mods now record relative paths in registry.
    However we still need the complete file-list in the registry
    to do this, *and* we need for remove to be able to understand
    relative paths.

--- CKAN/CKAN/Registry/InstalledModule.cs
+++ CKAN/CKAN/Registry/InstalledModule.cs
@@ -5,6 +5,7 @@ using System.IO;
 using Newtonsoft.Json;
 using System.Linq;
 using System.Runtime.Serialization;
+using log4net;
 
 namespace CKAN
 {
@@ -25,7 +26,7 @@ namespace CKAN
             }
         }
 
-        // TODO: Record path relative to KSP install
+        // TODO: Record our path (relative to KSP install)
         public InstalledModuleFile(string path)
         {
             this.sha1_sum = Sha1Sum(path);
@@ -76,6 +77,8 @@ namespace CKAN
         [JsonProperty] private DateTime install_time;
         [JsonProperty] private Module source_module;
 
+        private static readonly ILog log = LogManager.GetLogger(typeof(InstalledModule));
+
         // TODO: Our InstalledModuleFile already knows its path, so this could just
         // be a list. However we've left it as a dictionary for now to maintain
         // registry format compatibility.
@@ -107,19 +110,52 @@ namespace CKAN
 
         public InstalledModule(KSP ksp, Module module, IEnumerable<string> files)
         {
-            // TODO: Set all paths relative to the KSP path.
             this.install_time = DateTime.Now;
             this.source_module = module;
+            this.installed_files = new Dictionary<string, InstalledModuleFile>();
 
-            foreach (string file in files)
+            foreach (string full_path in files)
             {
-                installed_files[file] = new InstalledModuleFile(file);
+                string rel_path = MakeRelativeToKsp(full_path, ksp);
+
+                // IMF stil needs the full path for now so it can compute the SHA1
+                installed_files[rel_path] = new InstalledModuleFile(full_path);
             }
         }
 
         [JsonConstructor]
         private InstalledModule()
         {
+        }
+
+        /// <summary>
+        /// If the given file has an absolute path, make it relative to KSP.GameDir
+        /// Does nothing if the file is already relative.
+        /// Throws a kraken if it's relative, but not in the KSP dir.
+        /// </summary>
+        private static string MakeRelativeToKsp(string file, KSP ksp)
+        {
+            // If we've got an absolute path, convert it to a relative one
+            // from the GameRoot.
+            if (Path.IsPathRooted(file))
+            {
+                if (file.StartsWith(ksp.GameDir()))
+                {
+                    // The +1 here is because GameDir will never have
+                    // a trailing slash, but our path will.
+                    file = file.Remove(0, ksp.GameDir().Length + 1);
+                }
+                else
+                {
+                    throw new Kraken(
+                        String.Format(
+                            "Oh snap. {0} isn't inside my KSP dir ({1})",
+                            file, ksp.GameDir()
+                        )
+                    );
+                }
+            }
+            return file;
         }
     }
 }
