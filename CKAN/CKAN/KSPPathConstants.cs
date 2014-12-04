540184a1ef084cc05a9407307fe33236d977c371
Magnus Aagaard Sørensen <mgsdk@users.noreply.github.com>
2014-12-04 17:02:51 +0100

    Support for deleting a single registry key.

--- CKAN/CKAN/KSPPathConstants.cs
+++ CKAN/CKAN/KSPPathConstants.cs
@@ -5,7 +5,8 @@ namespace CKAN
 {
     public class KSPPathConstants
     {
-        public const string CKAN_KEY = @"HKEY_CURRENT_USER\Software\CKAN";
+        public const string CKAN_SUBKEY = @"Software\CKAN";
+        public const string CKAN_KEY = @"HKEY_CURRENT_USER\" + CKAN_SUBKEY;
         public const string CKAN_GAMEDIR_VALUE = @"GameDir";
         public const string CKAN_INSTANCES_COUNT_VALUE = @"InstancesCount";
         public static readonly string steamKSP = Path.Combine("SteamApps", "common", "Kerbal Space Program");
@@ -20,6 +21,30 @@ namespace CKAN
             return (T)Microsoft.Win32.Registry.GetValue(KSPPathConstants.CKAN_KEY, key, defaultValue);
         }
 
+        public static bool RemoveRegistryKey(string key)
+        {
+            // Check input.
+            if (key == null)
+            {
+                return false;
+            }
+
+            var _key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(KSPPathConstants.CKAN_SUBKEY, true);
+
+            if (_key == null)
+            {
+                return false;
+            }
+            else
+            {
+                _key.DeleteValue(key);
+            }
+
+            // Write the changes.
+            _key.Close();
+
+            return true;
+        }
     }
 }
 
