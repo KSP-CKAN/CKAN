f02990670cc44927f468d1af8ec51b129ad2c3da
Richard Lake <richard.lake@gmail.com>
2014-12-05 21:48:07 +1030

    Fixed bug and added more tests. Bus was in "sub_options = options". I assume the RROptions was a struct at some point?

--- CKAN/Tests/CKAN/Relationships/RelationshipResolver.cs
+++ CKAN/Tests/CKAN/Relationships/RelationshipResolver.cs
@@ -25,8 +25,8 @@ namespace Tests.CKAN.Relationships
         [Test]
         public void Constructor_WithoutModules_AlwaysReturns()
         {
-            var registry = Registry.Empty();
-            var options = RelationshipResolver.DefaultOpts();
+            registry = Registry.Empty();
+            options = RelationshipResolver.DefaultOpts();
             Assert.DoesNotThrow(() => new RelationshipResolver(new List<string>(),
                 options,
                 registry));
@@ -36,22 +36,119 @@ namespace Tests.CKAN.Relationships
         public void Constructor_WithConflictingModules_Throws()
         {
             var list = new List<string>();
-            var modA = generator.GeneratorRandomModule();
-            var modB = generator.GeneratorRandomModule(conflicts: new List<RelationshipDescriptor>
+            var mod_a = generator.GeneratorRandomModule();
+            var mod_b = generator.GeneratorRandomModule(conflicts: new List<RelationshipDescriptor>
             {
-                new RelationshipDescriptor {name=modA.identifier}
+                new RelationshipDescriptor {name=mod_a.identifier}
             });
 
-            list.Add(modA.identifier);
-            list.Add(modB.identifier);
-            registry.AddAvailable(modA);
-            registry.AddAvailable(modB);
+            list.Add(mod_a.identifier);
+            list.Add(mod_b.identifier);
+            AddToRegistry(mod_a, mod_b);
 
             Assert.Throws<InconsistentKraken>(() => new RelationshipResolver(
                 list,
                 options,
                 registry));
+        }
 
+        [Test]
+        public void Constructor_WithMultipleModulesProviding_Throws()
+        {
+            options.without_toomanyprovides_kraken = false;
+
+            var list = new List<string>();
+            var mod_a = generator.GeneratorRandomModule();
+            var mod_b = generator.GeneratorRandomModule(provides: new List<string>
+            {
+                mod_a.identifier
+            });
+            var mod_c = generator.GeneratorRandomModule(provides: new List<string>
+            {
+                mod_a.identifier
+            });
+            var mod_d = generator.GeneratorRandomModule(depends: new List<RelationshipDescriptor>
+            {
+                new RelationshipDescriptor {name=mod_a.identifier}
+            });
+
+            list.Add(mod_d.identifier);
+            AddToRegistry(mod_b, mod_c, mod_d);
+            Assert.Throws<TooManyModsProvideKraken>(() => new RelationshipResolver(
+                list,
+                options,
+                registry));
+
+        }
+
+        [Test]
+        public void Constructor_WithMissingModules_Throws()
+        {
+            var list = new List<string>();
+            var mod_a = generator.GeneratorRandomModule();
+            list.Add(mod_a.identifier);
+
+            Assert.Throws<ModuleNotFoundKraken>(() => new RelationshipResolver(
+                list,
+                options,
+                registry));
+
+        }
+
+        [Test]
+        [Ignore("Check if intended")]
+        public void ModList_WithInstalledModules_DoesNotContainThem()
+        {
+            var list = new List<string>();
+            var mod_a = generator.GeneratorRandomModule();
+            list.Add(mod_a.identifier);
+            AddToRegistry(mod_a);
+            registry.Installed().Add(mod_a.identifier, mod_a.version);
+
+            var relationship_resolver = new RelationshipResolver(list, options, registry);
+            CollectionAssert.IsEmpty(relationship_resolver.ModList());
+        }
+
+        [Test]
+        public void ModList_WithInstalledModulesSugested_DoesNotContainThem()
+        {
+            options.with_all_suggests = true;
+            var list = new List<string>();
+            var sugested = generator.GeneratorRandomModule();
+            var sugester = generator.GeneratorRandomModule(sugests: new List<RelationshipDescriptor>
+            {
+                new RelationshipDescriptor {name = sugested.identifier}
+            });
+
+            list.Add(sugester.identifier);
+            AddToRegistry(sugester, sugested);
+            registry.Installed().Add(sugested.identifier, sugested.version);
+
+            var relationship_resolver = new RelationshipResolver(list, options, registry);
+            CollectionAssert.Contains(relationship_resolver.ModList(), sugested);
+        }
+
+        [Test]
+        public void ModList_WithSugestedModulesThatWouldConflict_DoesNotContainThem()
+        {
+            options.with_all_suggests = true;
+            var list = new List<string>();
+            var sugested = generator.GeneratorRandomModule();
+            var mod = generator.GeneratorRandomModule(conflicts: new List<RelationshipDescriptor>
+            {
+                new RelationshipDescriptor(){name = sugested.identifier}
+            });
+            var sugester = generator.GeneratorRandomModule(sugests: new List<RelationshipDescriptor>
+            {
+                new RelationshipDescriptor {name = sugested.identifier}
+            });
+
+            list.Add(sugester.identifier);
+            list.Add(mod.identifier);
+            AddToRegistry(sugester, sugested, mod);
+
+            var relationship_resolver = new RelationshipResolver(list, options, registry);
+            CollectionAssert.DoesNotContain(relationship_resolver.ModList(), sugested);
         }
 
         [Test]
@@ -61,7 +158,7 @@ namespace Tests.CKAN.Relationships
             var dependant = generator.GeneratorRandomModule();
             var depender = generator.GeneratorRandomModule(depends: new List<RelationshipDescriptor>
             {
-                new RelationshipDescriptor(){name = dependant.identifier}
+                new RelationshipDescriptor {name = dependant.identifier}
             });
             var conflicts_with_dependant = generator.GeneratorRandomModule(conflicts: new List<RelationshipDescriptor>
             {
@@ -71,15 +168,56 @@ namespace Tests.CKAN.Relationships
 
             list.Add(depender.identifier);
             list.Add(conflicts_with_dependant.identifier);
-            registry.AddAvailable(depender);
-            registry.AddAvailable(dependant);
-            registry.AddAvailable(conflicts_with_dependant);
+            AddToRegistry(depender, dependant, conflicts_with_dependant);
 
             Assert.Throws<InconsistentKraken>(() => new RelationshipResolver(
                 list,
                 options,
                 registry));
         }
+        [Test]
+        public void Constructor_WithSuggests_HasSugestedInModlist()
+        {
+            options.with_all_suggests = true;
+            var list = new List<string>();
+            var sugested = generator.GeneratorRandomModule();
+            var sugester = generator.GeneratorRandomModule(sugests: new List<RelationshipDescriptor>
+            {
+                new RelationshipDescriptor {name = sugested.identifier}
+            });
+
+            list.Add(sugester.identifier);
+            AddToRegistry(sugester, sugested);
+
+            var relationship_resolver = new RelationshipResolver(list, options, registry);
+            CollectionAssert.Contains(relationship_resolver.ModList(), sugested);
+        }
+
+        [Test]
+        public void Constructor_ProvidesSatisfyDependencies()
+        {
+            var list = new List<string>();
+            var mod_a = generator.GeneratorRandomModule();
+            var mod_b = generator.GeneratorRandomModule(provides: new List<string>
+            {
+                mod_a.identifier
+            });
+            var depender = generator.GeneratorRandomModule(depends: new List<RelationshipDescriptor>
+            {
+                new RelationshipDescriptor {name = mod_a.identifier}
+            });
+            list.Add(depender.identifier);
+            AddToRegistry(mod_b, depender);
+            var relationship_resolver = new RelationshipResolver(list, options, registry);
+
+            CollectionAssert.AreEquivalent(relationship_resolver.ModList(), new List<CkanModule>
+            {
+                mod_b,
+                depender
+            });
+
+        }
+
 
         [Test]
         public void Constructor_WithMissingDependants_Throws()
@@ -88,7 +226,7 @@ namespace Tests.CKAN.Relationships
             var dependant = generator.GeneratorRandomModule();
             var depender = generator.GeneratorRandomModule(depends: new List<RelationshipDescriptor>
             {
-                new RelationshipDescriptor(){name = dependant.identifier}
+                new RelationshipDescriptor {name = dependant.identifier}
             });
             list.Add(depender.identifier);
             registry.AddAvailable(depender);
@@ -98,6 +236,14 @@ namespace Tests.CKAN.Relationships
                 options,
                 registry));
 
+        }
+
+        private void AddToRegistry(params CkanModule[] modules)
+        {
+            foreach (var module in modules)
+            {
+                registry.AddAvailable(module);
+            }
         }
     }
 }
