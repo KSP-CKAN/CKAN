2fe6a4b2b486342c9e9ed2c3140feb864691ec65
Richard Lake <richard.lake@gmail.com>
2014-12-07 09:16:02 +1030

    Moving some properties from GUIMod to CkanModule

--- CKAN/GUI/GUIMod.cs
+++ CKAN/GUI/GUIMod.cs
@@ -4,49 +4,93 @@ namespace CKAN
 {
     public class GUIMod
     {
+        private readonly Registry registry;
         private CkanModule Mod { get; set; }
-        public string Name{ get { return Mod.name; }}
-        public bool IsInstalled { get; private set; }
-        public bool HasUpdate { get; private set; }
-        public bool IsIncompatible { get; private set; }
-        public bool IsAutodetected { get; private set; }
-        public string Authors { get; private set; }
+        public string Name { get { return Mod.name; } }
+
+        public bool IsInstalled
+        {
+            get
+            {
+                Mod.UpdateModFieldsViaRegistry(registry);
+                return Mod.IsInstalled;
+            }
+        }
+
+        public bool HasUpdate
+        {
+            get
+            {
+                Mod.UpdateModFieldsViaRegistry(registry);
+                return Mod.HasUpdate;
+            }
+        }
+
+        public bool IsIncompatible
+        {
+            get
+            {
+                Mod.UpdateModFieldsViaRegistry(registry);
+                return Mod.IsIncompatible;
+            }
+        }
+
+        public bool IsAutodetected
+        {
+            get
+            {
+                Mod.UpdateModFieldsViaRegistry(registry);
+                return Mod.IsAutodetected;
+            }
+        }
+
+        public string Authors
+        {
+            get
+            {
+                return Mod.author == null ? "N/A" : String.Join(",", Mod.author);
+            }
+        }
+
         public string InstalledVersion { get; private set; }
         public string LatestVersion { get; private set; }
         public string KSPversion { get; private set; }
-        public string Abstract { get; private set; }
+
+        public string Abstract
+        {
+            get { return Mod.@abstract; }
+        }
+
         public object Homepage { get; private set; }
-        public string Identifier { get; private set; }
+
+        public string Identifier
+        {
+            get { return Mod.identifier; }
+        }
+
         public bool IsInstallChecked { get; set; }
         public bool IsUpgradeChecked { get; set; }
 
 
         public GUIMod(CkanModule mod, Registry registry)
         {
+            this.registry = registry;
             //Currently anything which could alter these causes a full reload of the modlist
             // If this is ever changed these could be moved into the properties
             Mod = mod;
-            IsInstalled = registry.IsInstalled(mod.identifier);
             IsInstallChecked = IsInstalled;
-            HasUpdate = IsInstalled && Mod.version.IsGreaterThan(registry.InstalledVersion(mod.identifier));
-            IsIncompatible = !registry.IsCompatible(mod.identifier, KSPManager.CurrentInstance.Version());
-            IsAutodetected = IsInstalled && registry.InstalledVersion(mod.identifier).ToString().Equals("autodetected dll");
-            Authors = mod.author == null ? "N/A" : String.Join(",", mod.author);
-            
-            var installedVersion = registry.InstalledVersion(mod.identifier);
-            var latestVersion = mod.version;
-            var kspVersion = mod.ksp_version;
 
-            InstalledVersion = installedVersion != null ? installedVersion.ToString() : "-";            
-            LatestVersion = latestVersion != null ? latestVersion.ToString() : "-";            
-            KSPversion = kspVersion != null ? kspVersion.ToString() : "-";
+            var installed_version = registry.InstalledVersion(mod.identifier);
+            var latest_version = mod.version;
+            var ksp_version = mod.ksp_version;
 
-            Abstract = mod.@abstract;
+            InstalledVersion = installed_version != null ? installed_version.ToString() : "-";
+            LatestVersion = latest_version != null ? latest_version.ToString() : "-";
+            KSPversion = ksp_version != null ? ksp_version.ToString() : "-";
+
             Homepage = mod.resources != null && mod.resources.homepage != null
-                ? (object) mod.resources.homepage
+                ? (object)mod.resources.homepage
                 : "N/A";
-
-            Identifier = mod.identifier;
         }
 
         public CkanModule ToCkanModule()
