412a8616e9e752a75e017d15381e7f1fcefceaef
Richard Lake <richard.lake@gmail.com>
2014-12-11 09:56:59 +1030

    Fixed issues raised in pull request by pjf. Removed isCompatible. Moved IsAutoDetected and HasUpdate to the registry.

--- CKAN/GUI/GUIMod.cs
+++ CKAN/GUI/GUIMod.cs
@@ -4,89 +4,49 @@ namespace CKAN
 {
     public class GUIMod
     {
-        private readonly Registry registry;
         private CkanModule Mod { get; set; }
         public string Name { get { return Mod.name; } }
-
-        public bool IsInstalled
-        {
-            get
-            {
-                return Mod.IsInstalled;
-            }
-        }
-
-        public bool HasUpdate
-        {
-            get
-            {
-                return Mod.HasUpdate;
-            }
-        }
-
-        public bool IsIncompatible
-        {
-            get
-            {
-                return Mod.IsIncompatible;
-            }
-        }
-
-        public bool IsAutodetected
-        {
-            get
-            {
-                return Mod.IsAutodetected;
-            }
-        }
-
-        public string Authors
-        {
-            get
-            {
-                return Mod.author == null ? "N/A" : String.Join(",", Mod.author);
-            }
-        }
-
+        public bool IsInstalled { get; private set; }
+        public bool HasUpdate { get; private set; }
+        public bool IsIncompatible { get; private set; }
+        public bool IsAutodetected { get; private set; }
+        public string Authors { get; private set; }
         public string InstalledVersion { get; private set; }
         public string LatestVersion { get; private set; }
         public string KSPversion { get; private set; }
-
-        public string Abstract
-        {
-            get { return Mod.@abstract; }
-        }
-
+        public string Abstract { get; private set; }
         public object Homepage { get; private set; }
-
-        public string Identifier
-        {
-            get { return Mod.identifier; }
-        }
-
+        public string Identifier { get; private set; }
         public bool IsInstallChecked { get; set; }
         public bool IsUpgradeChecked { get; set; }
 
 
         public GUIMod(CkanModule mod, Registry registry)
         {
-            this.registry = registry;
             //Currently anything which could alter these causes a full reload of the modlist
             // If this is ever changed these could be moved into the properties
             Mod = mod;
+            IsInstalled = registry.IsInstalled(mod.identifier);
             IsInstallChecked = IsInstalled;
+            HasUpdate = registry.HasUpdate(mod.identifier);
+            IsIncompatible = !mod.IsCompatibleKSP(KSPManager.CurrentInstance.Version());
+            IsAutodetected = registry.IsAutodetected(mod.identifier);
+            Authors = mod.author == null ? "N/A" : String.Join(",", mod.author);
 
-            var installed_version = registry.InstalledVersion(mod.identifier);
-            var latest_version = mod.version;
-            var ksp_version = mod.ksp_version;
+            var installedVersion = registry.InstalledVersion(mod.identifier);
+            var latestVersion = mod.version;
+            var kspVersion = mod.ksp_version;
 
-            InstalledVersion = installed_version != null ? installed_version.ToString() : "-";
-            LatestVersion = latest_version != null ? latest_version.ToString() : "-";
-            KSPversion = ksp_version != null ? ksp_version.ToString() : "-";
+            InstalledVersion = installedVersion != null ? installedVersion.ToString() : "-";
+            LatestVersion = latestVersion != null ? latestVersion.ToString() : "-";
+            KSPversion = kspVersion != null ? kspVersion.ToString() : "-";
 
+            Abstract = mod.@abstract;
             Homepage = mod.resources != null && mod.resources.homepage != null
                 ? (object)mod.resources.homepage
                 : "N/A";
+
+            Identifier = mod.identifier;
         }
 
         public CkanModule ToCkanModule()
