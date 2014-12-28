f847edde90325bf3f19b458207439e6e9ba840da
Nicolas Desseauve <nicolas@ormira.net>
2014-12-28 23:10:01 +0100

    Refactor module upgrade.

--- Action/Upgrade.cs
+++ Action/Upgrade.cs
@@ -30,47 +30,17 @@ namespace CKAN.CmdLine
                 return Exit.BADOPT;
             }
 
-            var to_upgrade = new List<CkanModule> ();
-
-            foreach (string mod in options.modules)
-            {
-                Match match = Regex.Match(mod, @"^(?<mod>[^=]*)=(?<version>.*)$");
-
-                if (match.Success)
-                {
-                    string ident = match.Groups["mod"].Value;
-                    string version = match.Groups["version"].Value;
-
-                    CkanModule module = ksp.Registry.GetModuleByVersion(ident, version);
-
-                    if (module == null)
-                    {
-                        User.RaiseMessage("Cannot install {0}, version {1} not available", ident, version);
-                        return Exit.ERROR;
-                    }
-
-                    to_upgrade.Add(module);
-                }
-                else
-                {
-                    to_upgrade.Add(
-                        ksp.Registry.LatestAvailable(mod, ksp.Version())
-                    );
-                }
-            }
-
-
-            var auto_detected_modules = to_upgrade.Where(module => ksp.Registry.IsAutodetected(module.identifier)).ToList();
-            foreach (var module in auto_detected_modules.Select(mod => mod.identifier))
-            {
-                User.RaiseMessage("Cannot upgrade {0} as it was not installed by CKAN. \n Please remove manually before trying again", module);
-            }
-            //Exit if any are auto_detected
-            if (auto_detected_modules.Any()) return Exit.ERROR;   
-
             User.RaiseMessage("\nUpgrading modules...\n");
             // TODO: These instances all need to go.
-            ModuleInstaller.GetInstance(ksp, User).Upgrade(to_upgrade,new NetAsyncDownloader(User));
+            try
+            {
+                ModuleInstaller.GetInstance(ksp, User).Upgrade(options.modules, new NetAsyncDownloader(User));
+            }
+            catch (ModuleNotFoundKraken kraken)
+            {
+                User.RaiseMessage(kraken.Message);
+                return Exit.ERROR;
+            }
             User.RaiseMessage("\nDone!\n");
             return Exit.OK;
 
