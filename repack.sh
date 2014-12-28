b13e011a3254d9a2b5ce22a994fc4a8763bf8059
Hakan Tandogan <hakan@gurkensalat.com>
2014-12-28 20:46:53 +0100

    Beautify linker script and test PR building

--- repack.sh
+++ repack.sh
@@ -1,3 +1,13 @@
 chmod a+x ../CKAN-core/packages/ILRepack.1.25.0/tools/ILRepack.exe
 
-mono ../CKAN-core/packages/ILRepack.1.25.0/tools/ILRepack.exe /target:exe /out:ckan.exe bin/Debug/CmdLine.exe bin/Debug/CKAN-GUI.exe bin/Debug/ChinhDo.Transactions.dll bin/Debug/CKAN.dll bin/Debug/CommandLine.dll bin/Debug/ICSharpCode.SharpZipLib.dll bin/Debug/log4net.dll bin/Debug/Newtonsoft.Json.dll
+mono ../CKAN-core/packages/ILRepack.1.25.0/tools/ILRepack.exe \
+	/target:exe \
+	/out:ckan.exe \
+	bin/Debug/CmdLine.exe \
+	bin/Debug/CKAN-GUI.exe \
+	bin/Debug/ChinhDo.Transactions.dll \
+	bin/Debug/CKAN.dll \
+	bin/Debug/CommandLine.dll \
+	bin/Debug/ICSharpCode.SharpZipLib.dll \
+	bin/Debug/log4net.dll \
+	bin/Debug/Newtonsoft.Json.dll
