755579488b751c33e93196f2d8378f4ab879d8e7
Hakan Tandogan <hakan@gurkensalat.com>
2014-12-29 19:08:20 +0100

    Keep the default repo Uri in the Repository class

--- Types/Repository.cs
+++ Types/Repository.cs
@@ -4,6 +4,9 @@ namespace CKAN
 {
     public class Repository
     {
+        [JsonIgnore] public static readonly Uri default_ckan_repo_name = new Uri("default");
+        [JsonIgnore] public static readonly Uri default_ckan_repo_uri = new Uri("https://github.com/KSP-CKAN/CKAN-meta/archive/master.zip");
+
         public string name;
         public Uri uri;
 
