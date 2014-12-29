d77c93e9a394a99050979357857269dd2955f696
Alexander Dzhoganov <alexander.dzhoganov@gmail.com>
2014-12-29 19:33:42 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ chmod a+x ../CKAN-core/packages/ILRepack.1.25.0/tools/ILRepack.exe
 
 mono ../CKAN-core/packages/ILRepack.1.25.0/tools/ILRepack.exe \
 	/target:exe \
-	/out:ckan.exe \
+	/out:../ckan.exe \
 	bin/Debug/CmdLine.exe \
 	bin/Debug/CKAN-GUI.exe \
 	bin/Debug/ChinhDo.Transactions.dll \
