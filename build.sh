692b9e012af1ae1b7bb83e3e3b7810cd411e708d
Alexander Dzhoganov <alexander.dzhoganov@gmail.com>
2014-12-29 19:44:14 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 xbuild /verbosity:minimal CKAN-netkan.sln
+chmod a+x ../CKAN-core/packages/ILRepack.1.25.0/tools/ILRepack.exe
+
+mono ../CKAN-core/packages/ILRepack.1.25.0/tools/ILRepack.exe /target:exe /out:netkan.exe bin/Debug/NetKAN.exe bin/Debug/log4net.dll CKAN-netkan/bin/Debug/Newtonsoft.Json.dll bin/Debug/ICSharpCode.SharpZipLib.dll bin/Debug/ChinhDo.Transactions.dll bin/Debug/CKAN.dll bin/Debug/CommandLine.dll bin/Debug/nunit.framework.dll
+
 nunit-console --exclude=FlakyNetwork Tests/bin/Debug/Tests.dll
 # prove
