14e3811e9765296e865b4fc6fb1ddd67219c8567
Hakan Tandogan <hakan@gurkensalat.com>
2014-12-31 15:12:23 +0100

    Skeleton for Multiple Repo unit tests

---
+++ Tests/CKAN/Registry/RegistryMultipleRepos.cs
@@ -0,0 +1,29 @@
+﻿using CKAN;
+using NUnit.Framework;
+using System;
+
+namespace CKANTests
+{
+    [TestFixture]
+    public class RegistryMultipleRepos
+    {
+        private CKAN.Registry registry;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Provide an empty registry before each test.
+            registry = CKAN.Registry.Empty();
+            Assert.IsNotNull(registry);
+        }
+
+        [Test]
+        public void Empty()
+        {
+            CKAN.Registry registry = CKAN.Registry.Empty();
+            Assert.IsInstanceOf<CKAN.Registry>(registry);
+        }
+
+    }
+}
+
