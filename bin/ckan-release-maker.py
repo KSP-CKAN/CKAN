22058351757ce7ff04aa857acf88cafd67a643be
Alexander Dzhoganov <alexander.dzhoganov@gmail.com>
2015-01-01 14:47:42 +0200

    refactored github utils into a python module

--- bin/ckan-release-maker.py
+++ bin/ckan-release-maker.py
@@ -4,71 +4,12 @@
 This script can create GitHub tags/ releases and push build artifacts to them.
 """
 
-GITHUB_API = 'https://api.github.com'
-
 # ---* DO NOT EDIT BELOW THIS LINE *---
 
 import os, sys
 import argparse
 
-import urllib
-import requests
-from urlparse import urljoin
-
-import datetime
-import base64
-
-import json
-
-def make_github_post_request(url_part, username, password, payload):
-    url = urljoin(GITHUB_API, url_part)
-    print '::make_github_post_request - %s' % url
-    return requests.post(url, auth = (username, password), data = json.dumps(payload), verify=False)
-
-def make_github_get_request(url_path, username, password, payload):
-    url = urljoin(GITHUB_API, url_path)
-    print '::make_github_get_request - %s' % url
-    return requests.get(url, auth = (username, password), data = json.dumps(payload), verify=False)
-
-def make_github_put_request(url_path, username, password, payload):
-    url = urljoin(GITHUB_API, url_path)
-    print '::make_github_put_request - %s' % url
-    return requests.put(url, auth = (username, password), data = json.dumps(payload), verify=False)
-
-def make_github_post_request_raw(url_part, username, password, payload, content_type):
-    url = urljoin(GITHUB_API, url_part)
-    print '::make_github_post_request_raw - %s' % url
-
-    headers = { 'Content-Type': content_type }
-    return requests.post(url, auth = (username, password), data = payload, verify=False, headers=headers)
-
-def make_github_release(username, password, repo, tag_name, name, body, draft, prerelease):
-    payload = {}
-    payload['tag_name'] = tag_name
-    payload['name'] = name
-    payload['body'] = body
-    payload['draft'] = draft
-    payload['prerelease'] = prerelease
-    return make_github_post_request('/repos/%s/releases' % repo, username, password, payload)
-
-def make_github_release_artifact(username, password, upload_url, filepath, content_type = 'application/zip'):
-    filename = os.path.basename(filepath)
-    query = { 'name': filename }
-    url = '%s?%s' % (upload_url[:-7], urllib.urlencode(query))
-    payload = file(filepath, 'r').read()
-    return make_github_post_request_raw(url, username, password, payload, content_type)
-
-def get_github_file(username, password, repo, path):
-    return make_github_get_request('/repos/%s/contents/%s' % (repo, path), username, password, {})
-
-def push_github_file(username, password, repo, path, sha, content, branch='master'):
-    payload = {}
-    payload['path'] = path
-    payload['message'] = 'Updating build-tag'
-    payload['content'] = base64.b64encode(content)
-    payload['sha'] = sha
-    payload['branch'] = branch
-    return make_github_put_request('/repos/%s/contents/%s' % (repo, path), username, password, payload)
+from ckan_github_utils import *
 
 def main():
     parser = argparse.ArgumentParser(description='Create GitHub releases and upload build artifacts')
---
+++ bin/ckan-release-promoter.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from ckan_github_utils import *
+
+import os, sys
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
