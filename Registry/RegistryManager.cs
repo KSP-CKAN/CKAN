ea77b082a8107dd868b01b64e2c47ff37293a5ee
Hakan Tandogan <hakan@gurkensalat.com>
2015-01-07 13:31:13 +0100

    Export a ckan file with dependencies to installed mods

--- Registry/RegistryManager.cs
+++ Registry/RegistryManager.cs
@@ -147,8 +147,34 @@ namespace CKAN
 
         private string SerializeCurrentInstall()
         {
+            // TODO how do we obtain the name of the current KSP instance?
+            string kspInstanceName = "default";
+            string name = "installed-" + kspInstanceName;
+
             JObject installed = new JObject ();
-            installed ["foo"] = "bar";
+            installed["spec_version"] = "1";
+            installed["identifier"] = name;
+            installed["version"] =  DateTime.UtcNow.ToString("yyyy.MM.dd.hh.mm.ss");
+
+            installed["license"] = "unknown";
+            installed["name"] = name;
+            installed["abstract"] = "A list of modules installed on the " + kspInstanceName + " KSP instance";
+            installed["download"] = "http://munich.ksp-ckan.org/empty-0.0.0.zip";
+
+            JArray depends = new JArray ();
+            installed["depends"] = depends;
+
+            foreach(KeyValuePair<string, CKAN.Version> module in registry.Installed())
+            {
+                if (!(CKAN.Version.AutodetectedDllString.Equals(module.Value.ToString())))
+                {
+                    JObject moduleJson = new JObject();
+                    moduleJson["name"] = module.Key;
+                    moduleJson["version"] = module.Value.ToString();
+
+                    depends.Add(moduleJson);
+                }
+            }
 
             StringBuilder sb = new StringBuilder();
             StringWriter sw = new StringWriter(sb);
