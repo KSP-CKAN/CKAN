38a8b28c262cf0defb1e3541703d6f803a6be3fe
Alexander Dzhoganov <alexander.dzhoganov@gmail.com>
2015-03-05 18:18:01 +0200

    Small dialog fix

--- NewUpdateDialog.Designer.cs
+++ NewUpdateDialog.Designer.cs
@@ -30,7 +30,6 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.VersionLabel = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.ReleaseNotesTextbox = new System.Windows.Forms.TextBox();
             this.InstallUpdateButton = new System.Windows.Forms.Button();
             this.CancelUpdateButton = new System.Windows.Forms.Button();
@@ -40,7 +39,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(9, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(53, 13);
             this.label1.TabIndex = 0;
@@ -50,31 +49,22 @@
             // 
             this.VersionLabel.AutoSize = true;
             this.VersionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.VersionLabel.Location = new System.Drawing.Point(71, 9);
+            this.VersionLabel.Location = new System.Drawing.Point(68, 9);
             this.VersionLabel.Name = "VersionLabel";
             this.VersionLabel.Size = new System.Drawing.Size(43, 13);
             this.VersionLabel.TabIndex = 1;
             this.VersionLabel.Text = "v0.0.0";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 36);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(52, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Changes:";
             // 
             // ReleaseNotesTextbox
             // 
             this.ReleaseNotesTextbox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.ReleaseNotesTextbox.Location = new System.Drawing.Point(12, 52);
+            this.ReleaseNotesTextbox.Location = new System.Drawing.Point(12, 25);
             this.ReleaseNotesTextbox.Multiline = true;
             this.ReleaseNotesTextbox.Name = "ReleaseNotesTextbox";
             this.ReleaseNotesTextbox.ReadOnly = true;
-            this.ReleaseNotesTextbox.Size = new System.Drawing.Size(402, 219);
+            this.ReleaseNotesTextbox.Size = new System.Drawing.Size(402, 246);
             this.ReleaseNotesTextbox.TabIndex = 3;
             // 
             // InstallUpdateButton
@@ -109,7 +99,6 @@
             this.Controls.Add(this.CancelUpdateButton);
             this.Controls.Add(this.InstallUpdateButton);
             this.Controls.Add(this.ReleaseNotesTextbox);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.VersionLabel);
             this.Controls.Add(this.label1);
             this.MinimumSize = new System.Drawing.Size(354, 245);
@@ -124,7 +113,6 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label VersionLabel;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox ReleaseNotesTextbox;
         private System.Windows.Forms.Button InstallUpdateButton;
         private System.Windows.Forms.Button CancelUpdateButton;
