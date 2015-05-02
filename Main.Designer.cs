8227ea332f2b0c40f23fdb953eefba41e8b818b6
Richard Lake <richard.lake@gmail.com>
2015-05-02 12:19:24 +0930

    Set the ModInfo size to be fixed when changing the size of the full screen.

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -445,6 +445,7 @@ namespace CKAN
             this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
             this.splitContainer1.Location = new System.Drawing.Point(0, 72);
             this.splitContainer1.Name = "splitContainer1";
             // 
