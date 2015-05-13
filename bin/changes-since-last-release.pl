274db24781f739d13aaa0dbc319b6c2bb4e502eb
Paul Fenwick <pjf@perltraining.com.au>
2015-05-13 18:30:11 +1000

    Add changes-since-last-release.pl
    This nifty little script can be used under the current build
    system to see the changes since the last release.
    
    Not used by our users at all, and not a required tool for our
    contributors, but damn useful for me when making releases.

---
+++ bin/changes-since-last-release.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/perl -w
+use 5.010;
+use strict;
+use warnings;
+use autodie qw(:all);
+use FindBin qw($Bin);
+use Getopt::Std;
+
+# Shows everything which changed since the last release.
+# This should be easy, but the current build processes don't
+# actually tag the repos. So we have to look in the `build-tag`
+# file. Still a better love story than Twilight.
+
+my %opts = (
+    'f' => 0,           # Fetch from upstream
+    'u' => 'origin',    # Upstream name
+);
+
+# Why doesn't C# have something this easy for option handling?
+
+getopts('fu:', \%opts);
+
+open(my $fh, '<', "$Bin/../build-tag");
+
+my %last_release_commit;
+
+my $tags = <$fh>;
+
+foreach my $segment (split(/\|/,$tags)) {
+    $segment =~ /^(?<repo>[^+]+)\+(?<commit>[0-9a-f]+)$/
+        or die "Cannot parse $segment";
+
+    $last_release_commit{$+{repo}} = $+{commit};
+}
+
+# Walk through each repo and display changes.
+
+foreach my $repo (keys %last_release_commit) {
+    chdir("$Bin/../$repo");
+
+    say "\n== $repo ==\n";
+
+    system("git fetch origin") if $opts{f};
+
+    # By piping to cat, we avoid git invoking the pager.
+    system("git log --no-merges --pretty=oneline --abbrev-commit $last_release_commit{$repo}..$opts{u}/master ");
+}
