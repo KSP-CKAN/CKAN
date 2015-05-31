755a5a640c603563c6adae3b5aad84502a4c5cf4
Adrian Todd <solarshado@gmail.com>
2015-05-30 22:18:52 -0400

    Set ChooseKSPInstance's AcceptButton to the SelectButton, so you can easily select an instance with the arrow keys + enter

--- GUI/ChooseKSPInstance.Designer.cs
+++ GUI/ChooseKSPInstance.Designer.cs
@@ -143,6 +143,7 @@
             // 
             // ChooseKSPInstance
             // 
+            this.AcceptButton = this.SelectButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(486, 355);
