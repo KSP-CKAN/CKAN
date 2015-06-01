9cad410587eba154c3eeed94e2134f31a4872d86
Richard Lake <richard.lake@gmail.com>
2015-06-01 22:08:09 +0930

    Remove unused method from auto updater.
    Closes https://github.com/KSP-CKAN/CKAN/issues/1041

--- AutoUpdate/Main.cs
+++ AutoUpdate/Main.cs
@@ -6,10 +6,10 @@ using System.Threading;
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * CKAN AUTO-UPDATE TOOL
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * 
+ *
  * This simple program is used to replace the local ckan.exe with the latest one i.e. auto-update.
  * It is a command-line tool only, meant to be invoked by the main CKAN process and not manually.
- * 
+ *
  * Invoked as:
  * AutoUpdate.exe <running CKAN PID> <running CKAN path> <updated CKAN path>
  */
@@ -67,31 +67,14 @@ namespace AutoUpdater
                     return;
                 }
             }
-           
+
             // replace ckan.exe
             File.Move(updated_path, local_path);
 
             if (args[3] == "launch")
             {
                 //Start CKAN
-                Process.Start(local_path);    
-            }
-        }
-
-        public static bool IsLinux
-        {
-            get
-            {
-                // Magic numbers ahoy! This arcane incantation was found
-                // in a Unity help-page, which was found on a scroll,
-                // which was found in an urn that dated back to Mono 2.0.
-                // It documents singular numbers of great power.
-                //
-                // "And lo! 'pon the 4, 6, and 128 the penguin shall
-                // come, and it infiltrate dominate from the smallest phone to
-                // the largest cloud."
-                int p = (int)Environment.OSVersion.Platform;
-                return (p == 4) || (p == 6) || (p == 128);
+                Process.Start(local_path);
             }
         }
     }
