b25cd7c8103a52fc8e46bcc164204f4c6b66bd36
Dwayne Bent <dbb@dbb.io>
2015-06-11 10:05:33 -0400

    Update doc comments

--- Core/Exporters/IExporter.cs
+++ Core/Exporters/IExporter.cs
@@ -3,15 +3,15 @@
 namespace CKAN.Exporters
 {
     /// <summary>
-    /// Represents an object that can 
+    /// Represents an object that can export a list of mods in a machine/human readable text format.
     /// </summary>
     public interface IExporter
     {
         /// <summary>
-        /// 
+        /// Export the installed mods.
         /// </summary>
-        /// <param name="registry"></param>
-        /// <param name="writer"></param>
+        /// <param name="registry">The registry of mods to be exported.</param>
+        /// <param name="writer">The text writer to write the export to.</param>
         void Export(Registry registry, TextWriter writer);
     }
 }
