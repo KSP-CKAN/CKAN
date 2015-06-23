87b6236e1250394f8c8b88bd2699f9104ae844da
Richard Lake <richard.lake@gmail.com>
2015-06-23 18:45:33 +0930

    Extract IDownloader from NetAsyncDownloader

---
+++ Core/Net/IDownloader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace CKAN
+{
+    public interface IDownloader
+    {
+        /// <summary>
+        /// Downloads all the modules specified to the cache.
+        /// Even if modules share download URLs, they will only be downloaded once.
+        /// Blocks until the downloads are complete, cancelled, or errored.
+        /// </summary>
+        void DownloadModules(NetFileCache cache, IEnumerable<CkanModule> modules);
+
+        /// <summary>
+        /// Cancel any running downloads.
+        /// </summary>
+        void CancelDownload();
+    }
+}
