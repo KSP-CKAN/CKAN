90ab7a970de7bc6ac05de8a7a29b06b88b7a157c
Paul Fenwick <pjf@perltraining.com.au>
2015-06-23 11:38:00 +1000

    RelationshipDescriptor: Failing test for version_within_bounds + AD
    Autodetected mods should satisfy *any* relationship. These test cases
    show the presence of #1154.

--- Tests/Core/Types/RelationshipDescriptor.cs
+++ Tests/Core/Types/RelationshipDescriptor.cs
@@ -7,12 +7,56 @@ namespace Tests.Core.Types
     [TestFixture]
     public class RelationshipDescriptor
     {
-        [Test]
-        public void VersionWithinBounds_Exact()
-        {
-            var rd = new CKAN.RelationshipDescriptor { version = new CKAN.Version("0.23") };
 
-            Assert.That(rd.version_within_bounds(new CKAN.Version("0.23")));
+        CKAN.Version autodetected = new CKAN.DllVersion();
+
+        [Test]
+        [TestCase("0.23","0.23", true)]
+        [TestCase("wibble","wibble",true)]
+        [TestCase("0.23", "0.23.1", false)]
+        [TestCase("wibble","wobble", false)]
+        public void VersionWithinBounds_ExactFalse(string version, string other_version, bool expected)
+        {
+            var rd = new CKAN.RelationshipDescriptor { version = new CKAN.Version(version) };
+            Assert.AreEqual(expected, rd.version_within_bounds(new CKAN.Version(other_version)));
+        }
+
+        [Test]
+        [TestCase("0.20","0.23","0.21", true)]
+        [TestCase("0.20","0.23","0.20", true)]
+        [TestCase("0.20","0.23","0.23", true)]
+        public void VersionWithinBounds_MinMax(string min, string max, string compare_to, bool expected)
+        {
+            var rd = new CKAN.RelationshipDescriptor
+            {
+                min_version = new CKAN.Version(min),
+                max_version = new CKAN.Version(max)
+            };
+
+            Assert.AreEqual(expected, rd.version_within_bounds(new CKAN.Version(compare_to)));
+        }
+
+        [Test]
+        [TestCase("0.23")]
+        public void VersionWithinBounds_vs_AutoDetectedMod(string version)
+        {
+            var rd = new CKAN.RelationshipDescriptor { version = new CKAN.Version(version) };
+
+            Assert.True(rd.version_within_bounds(autodetected));
+        }
+
+        [Test]
+        [TestCase("0.20","0.23")]
+        public void VersionWithinBounds_MinMax_vs_AutoDetectedMod(string min, string max)
+        {
+            var rd = new CKAN.RelationshipDescriptor
+            {
+                min_version = new CKAN.Version(min),
+                max_version = new CKAN.Version(max)
+            };
+            
+            Assert.True(rd.version_within_bounds(autodetected));
+
         }
     }
 }
