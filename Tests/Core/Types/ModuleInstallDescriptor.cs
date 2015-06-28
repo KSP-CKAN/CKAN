0c1f5fb214448f9342ce5f9e794b5f0e864245c1
Paul Fenwick <pjf@perltraining.com.au>
2015-06-28 21:22:19 +1000

    Test cases on find_regexp
    Part of #1089.
    
    Written while tired, so not very elegant.

--- Tests/Core/Types/ModuleInstallDescriptor.cs
+++ Tests/Core/Types/ModuleInstallDescriptor.cs
@@ -2,6 +2,9 @@ using System.Collections.Generic;
 using CKAN;
 using NUnit.Framework;
 using Tests.Data;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using ICSharpCode.SharpZipLib.Zip;
 
 namespace Tests.Core.Types
 {
@@ -32,6 +35,24 @@ namespace Tests.Core.Types
                 test_filter(stanza.filter, "Firespitter/filter");
                 test_filter(stanza.filter_regexp, "Firespitter/filter_regexp");
             }
+        }
+
+        [Test]
+        [TestCase("/DogeCoinFlag", "DogeCoinFlag-1.01/GameData/DogeCoinFlag","Anchored")]
+        [TestCase("DogeCoinFlag", "DogeCoinFlag-1.01","Missing anchors")]
+        public void regexp_filter_1089(string regexp, string expected, string comment)
+        {
+            string json = @"{
+                ""install_to""  : ""GameData"",
+                ""find_regexp"" : """ + regexp + @"""
+            }"; 
+
+            var mid = JsonConvert.DeserializeObject<CKAN.ModuleInstallDescriptor>(json);
+            var zip = new ZipFile(TestData.DogeCoinFlagZipWithExtras());
+
+            mid = mid.ConvertFindToFile(zip);
+
+            Assert.AreEqual(expected, mid.file, comment);
         }
 
         private static void test_filter(List<string> filter, string message)
