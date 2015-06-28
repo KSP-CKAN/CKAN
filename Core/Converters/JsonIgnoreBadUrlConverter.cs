5f9002614e94d39d75f87da3ee19dc2109697c4e
Paul Fenwick <pjf@perltraining.com.au>
2015-06-28 16:09:57 +1000

    Modules now ignore bad URLs in resources.
    This logs them and moves on, rather than causing everything to die.
    
    Fixes #1208.

---
+++ Core/Converters/JsonIgnoreBadUrlConverter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using log4net;
+
+namespace CKAN
+{
+    /// <summary>
+    /// Reads a URL string, and returns a Uri object. Returns null if the string
+    /// doesn't parse.
+    /// </summary>
+    public class JsonIgnoreBadUrlConverter : JsonConverter
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(JsonIgnoreBadUrlConverter));
+
+        public override object ReadJson(
+            JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            string value = reader.Value == null ? null : reader.Value.ToString();
+
+            Uri url;
+
+            try
+            {
+                url = new Uri(value);
+                return url;
+            }
+            catch
+            {
+                log.WarnFormat("{0} is not a valid URL, ignoring", value);
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Opt out of converting any types, except those we've been specifically applied to.
+        /// </summary>
+        public override bool CanConvert(Type object_type)
+        {
+            return false;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
