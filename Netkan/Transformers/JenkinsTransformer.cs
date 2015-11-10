cc00d7b77940a44873d80e76c8175322fe810e69
Dwayne Bent <dbb@dbb.io>
2015-11-10 10:01:20 -0500

    More robust Jenkins support

--- Netkan/Transformers/JenkinsTransformer.cs
+++ Netkan/Transformers/JenkinsTransformer.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using CKAN.NetKAN.Extensions;
 using CKAN.NetKAN.Model;
+using CKAN.NetKAN.Services;
 using CKAN.NetKAN.Sources.Jenkins;
 using log4net;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace CKAN.NetKAN.Transformers
@@ -14,6 +18,24 @@ namespace CKAN.NetKAN.Transformers
     {
         private static readonly ILog Log = LogManager.GetLogger(typeof(JenkinsTransformer));
 
+        private static readonly Dictionary<string, string> BuildTypeToProperty = new Dictionary<string, string>
+        {
+            { "any", "lastBuild" },
+            { "compeleted", "lastCompletedBuild" },
+            { "failed", "lastFailedBuild" },
+            { "stable", "lastStableBuild" },
+            { "successful", "lastSuccessfulBuild" },
+            { "unstable", "lastUnstableBuild" },
+            { "unsuccessful", "lastUnsuccessfulBuild" }
+        };
+
+        private readonly IHttpService _http;
+
+        public JenkinsTransformer(IHttpService http)
+        {
+            _http = http;
+        }
+
         public Metadata Transform(Metadata metadata)
         {
             if (metadata.Kref != null && metadata.Kref.Source == "jenkins")
@@ -23,16 +45,58 @@ namespace CKAN.NetKAN.Transformers
                 Log.InfoFormat("Executing Jenkins transformation with {0}", metadata.Kref);
                 Log.DebugFormat("Input metadata:{0}{1}", Environment.NewLine, json);
 
-                var versionBase = (string)json["x_ci_version_base"];
-                var resources = (JObject)json["resources"];
-                var baseUri = (string)resources["ci"] ?? (string)resources["x_ci"];
+                var buildType = "stable";
 
-                var build = JenkinsAPI.GetLatestBuild(baseUri, versionBase);
+                var jenkinsMetadata = (JObject)json["x_netkan_jenkins"];
+                if (jenkinsMetadata != null)
+                {
+                    var jenkinsBuildMetadata = (string)jenkinsMetadata["build"];
 
-                Log.DebugFormat("Found Jenkins Mod: {0} {1}", metadata.Kref.Id, build.version);
+                    if (jenkinsBuildMetadata != null)
+                    {
+                        buildType = jenkinsBuildMetadata;
+                    }
+                }
 
-                json.SafeAdd("version", build.version.ToString());
-                json.SafeAdd("download", Uri.EscapeUriString(build.download.ToString()));
+                Log.InfoFormat("Attempting to retrieve the last {0} build", buildType);
+
+                // Get the job metadata
+                var job = JsonConvert.DeserializeObject<JObject>(
+                    _http.DownloadText(new Uri(metadata.Kref.Id + "api/json"))
+                );
+
+                // Get the build reference metadata
+                var buildRef = (JObject)job[BuildTypeToProperty[buildType]];
+
+                // Get the build number and url
+                var buildNumber = (int)buildRef["number"];
+                var buildUrl = (string)buildRef["url"];
+
+                Log.InfoFormat("The last {0} build is #{1}", buildType, buildNumber);
+
+                // Get the build metadata
+                var build = JsonConvert.DeserializeObject<JObject>(
+                    _http.DownloadText(new Uri(buildUrl + "api/json"))
+                );
+
+                // Get the artifact metadata
+                // TODO: Support asset_matching
+                var artifact = ((JArray)build["artifacts"])
+                    .Select(i => (JObject)i)
+                    .Single(i => ((string)i["fileName"]).ToLowerInvariant().EndsWith(".zip"));
+
+                var artifactFileName = artifact["fileName"];
+                var artifactRelativePath = artifact["relativePath"];
+
+                // I'm not sure if 'relativePath' is the right property to use here
+                var download = Uri.EscapeUriString(buildUrl + "artifact/" + artifactRelativePath);
+                var version = artifactFileName;
+
+                Log.DebugFormat("Using download URL: {0}", download);
+                Log.DebugFormat("Using version: {0}", version);
+
+                json.SafeAdd("download", download);
+                json.SafeAdd("version", version);
 
                 Log.DebugFormat("Transformed metadata:{0}{1}", Environment.NewLine, json);
 
