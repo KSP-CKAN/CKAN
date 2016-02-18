0a866984fac9d3712ab7d0aabeb8d28070eb0c21
ChucklesTheBeard <ChucklesTheBeard+git@gmail.com>
2016-02-17 19:14:33 -0500

    add TorrentDownloader stub

---
+++ Core/Net/TorrentDownloader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CKAN
+{
+    public class TorrentDownloader : IDownloader
+    {
+        public IUser User { get; set; }
+
+        public TorrentDownloader(IUser user)
+        {
+            User = user;
+        }
+        void DownloadModules(NetFileCache cache, IEnumerable<CkanModule> modules);
+        void CancelDownload();
+    }
+}
+
