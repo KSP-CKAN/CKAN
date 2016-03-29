80c3c9217af9caaa1308bb30370b3d3a0b26e1ff
Jack Twilley <mathuin@gmail.com>
2016-03-29 16:43:04 -0700

    Added more output to entrypoint.sh
    The entrypoint script now runs the "scan" and "list" commands to inform the
    user with regard to existing mods before running the "update" and "upgrade"
    commands.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@ HEAD="mono /build/CmdLine.exe"
 TAIL="--asroot --headless"
 $HEAD ksp add one /kspdir $TAIL
 $HEAD ksp default one $TAIL
+$HEAD scan $TAIL
+$HEAD list $TAIL
 $HEAD update $TAIL
 $HEAD upgrade --all $TAIL
 chown --reference=/kspdir/GameData -R /kspdir
