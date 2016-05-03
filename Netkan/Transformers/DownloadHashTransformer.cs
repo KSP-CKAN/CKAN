a30b024c9462605647b469d26ca2f49925c10e8b
Leon Wright <techman83@gmail.com>
2016-05-04 01:19:06 +0800

    Change array for "type":"digest" objects

--- Netkan/Transformers/DownloadHashTransformer.cs
+++ Netkan/Transformers/DownloadHashTransformer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using CKAN.NetKAN.Model;
 using CKAN.NetKAN.Services;
+using CKAN.NetKAN.Extensions;
 using log4net;
 using Newtonsoft.Json.Linq;
 
@@ -37,20 +38,11 @@ namespace CKAN.NetKAN.Transformers
 
                 if (file != null)
                 {
-                    var sha1 = new JObject();
-                    sha1.Add("type", "sha1");
-                    sha1.Add("digest", _fileService.GetFileHashSha1(file));
-                    
-                    var sha256 = new JObject();
-                    sha256.Add("type", "sha256");
-                    sha256.Add("digest", _fileService.GetFileHashSha256(file));
-                    
-                    json["download_hash"] = new JArray();
+                    json["download_hash"] = new JObject();
 
-                    var download_hashJson = (JArray)json["download_hash"];
-
-                    download_hashJson.Add(sha1);
-                    download_hashJson.Add(sha256);
+                    var download_hashJson = (JObject)json["download_hash"];
+                    download_hashJson.SafeAdd("sha1", _fileService.GetFileHashSha1(file));
+                    download_hashJson.SafeAdd("sha256", _fileService.GetFileHashSha256(file));
                 }
 
                 Log.DebugFormat("Transformed metadata:{0}{1}", Environment.NewLine, json);
