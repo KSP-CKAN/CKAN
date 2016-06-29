d05b9a58561fc6deee573c16030ad1323bd5d62e
Martin Panzer <postremus1996@googlemail.com>
2016-06-29 20:29:24 +0200

    Ignore ksp instances if we are launching the gui anyway fixes #1785

--- Cmdline/Main.cs
+++ Cmdline/Main.cs
@@ -141,7 +141,7 @@ This is a bad idea and there is absolutely no good reason to do it. Please run C
                 // Set a KSP directory by its path
                 manager.SetCurrentInstanceByPath(options.KSPdir);
             }
-            else if (! (cmdline.action == "ksp" || cmdline.action == "version"))
+            else if (! (cmdline.action == "ksp" || cmdline.action == "version" || cmdline.action == "gui"))
             {
                 // Find whatever our preferred instance is.
                 // We don't do this on `ksp/version` commands, they don't need it.
