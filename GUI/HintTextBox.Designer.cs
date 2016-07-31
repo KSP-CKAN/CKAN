d42e14666a0cf7127e10a70f6789c54dc2eb38aa
ayan4m1 <andrew@bulletlogic.com>
2016-07-31 10:00:47 -0400

    add subclass of TextBox to handle clear icon

---
+++ GUI/HintTextBox.Designer.cs
@@ -0,0 +1,44 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace CKAN
+{
+    partial class HintTextBox
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+        
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // HintTextBox
+            // 
+            this.TextChanged += new System.EventHandler(this.HintTextBox_TextChanged);
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ GUI/HintTextBox.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CKAN
+{
+    public partial class HintTextBox : TextBox
+    {
+        public HintTextBox()
+        {
+            InitializeComponent();
+        }
+
+        private void HintTextBox_TextChanged(object sender, EventArgs e)
+        {
+            // sanity checks
+            if (!Visible || ReadOnly)
+            {
+                return;
+            }
+
+            if (TextLength <= 0)
+            {
+                return;
+            }
+
+        }
+    }
+}
