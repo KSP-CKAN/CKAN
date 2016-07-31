7b8255a511716c07fc4d305f436d393bec76da8b
ayan4m1 <andrew@bulletlogic.com>
2016-07-31 10:00:02 -0400

    fix up resource binding for clear icon

--- GUI/Properties/Resources.Designer.cs
+++ GUI/Properties/Resources.Designer.cs
@@ -151,6 +151,16 @@ namespace CKAN.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap textClear {
+            get {
+                object obj = ResourceManager.GetObject("textClear", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap update {
             get {
                 object obj = ResourceManager.GetObject("update", resourceCulture);
