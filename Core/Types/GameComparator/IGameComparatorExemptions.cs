160b1193d5310dfef822fd213efe60689c030cf1
Rusk85 <troggs@gmx.net>
2016-12-24 01:38:50 +0100

    Added way of waving through certain modules in terms of version compatibility by pronouncing them compatible to each and every KSP version. This was done due to Strategia missing from CKAN and its dependency CustomBarnKit officially not being supported by the latest build of KSP while in fact it is. The way this waving process selects eligible modules is hardcoded but can be easily made more dynamic.

---
+++ Core/Types/GameComparator/IGameComparatorExemptions.cs
@@ -0,0 +1,11 @@
+﻿namespace CKAN
+{
+    /// <summary>
+    /// Allows certain modules to pass as compatible even though
+    /// they might not be.
+    /// </summary>
+    public interface IGameComparatorExemptions
+    {
+        bool IsExempt(CkanModule module);
+    }
+}
