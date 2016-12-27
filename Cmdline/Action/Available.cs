2fe5ad78e94934dec3ca5912b890a35af5b0bd2a
Myk <politas@gmail.com>
2016-12-27 16:29:21 +1100

    Remove useless padding code

--- Cmdline/Action/Available.cs
+++ Cmdline/Action/Available.cs
@@ -1,18 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text.RegularExpressions;
-using CKAN.Exporters;
-using CKAN.Types;
-using log4net;
+//using System.Linq;
+//using System.Text.RegularExpressions;
+//using CKAN.Exporters;
+//using CKAN.Types;
+//using log4net;
 
 namespace CKAN.CmdLine
 {
     public class Available : ICommand
     {
-        private static readonly ILog log = LogManager.GetLogger(typeof(List));
-
         public IUser user { get; set; }
 
         public Available(IUser user)
@@ -20,27 +18,9 @@ namespace CKAN.CmdLine
             this.user = user;
         }
 
-        private int ConsoleWidth()
-        {
-            try
-            {
-                var width = user.WindowWidth;
-                log.InfoFormat("Found console width {0}", width);
-                return width;
-            }
-            catch (Kraken)
-            {
-                var width = 80;
-                log.InfoFormat("Could not find console width, defaulting to {0}", width);
-                return width;
-            }
-        }
-
         public int RunCommand(CKAN.KSP ksp, object raw_options)
         {
             IRegistryQuerier registry = RegistryManager.Instance(ksp).registry;
-
-            var width = ConsoleWidth();
 
             List<CkanModule> available = registry.Available(ksp.VersionCriteria());
 
@@ -49,8 +29,7 @@ namespace CKAN.CmdLine
 
             foreach (CkanModule module in available)
             {
-                    string entry = String.Format("* {0} ({1}) - {2}", module.identifier, module.version, module.name);
-                    user.RaiseMessage(width > 0 ? entry.PadRight(width).Substring(0, width - 1) : entry);
+                user.RaiseMessage(String.Format("* {0} ({1}) - {2}", module.identifier, module.version, module.name));
             }
 
             return Exit.OK;
