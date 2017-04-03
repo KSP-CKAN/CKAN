6bf2cc468496d4150aa1ca0bc807b0238c880adf
Myk <politas@gmail.com>
2017-04-03 11:53:56 +1000

    Adding Chicken Bits

--- Core/Net/NetAsyncDownloader.cs
+++ Core/Net/NetAsyncDownloader.cs
@@ -83,7 +83,14 @@ namespace CKAN
                 downloads.Add(download);
             }
 
-            DownloadNative();
+            // adding chicken bits
+            if (Platform.IsWindows || System.Environment.GetEnvironmentVariable("KSP_CKAN_USE_CURL") != null) {
+                DownloadNative();
+            }
+            else
+            {
+                DownloadCurl();
+            }
 
         }
 
