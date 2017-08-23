1a24f8d66a35f4dd60604775049574e0f0fd8cf0
J/A <archer884@gmail.com>
2017-08-23 14:33:42 -0500

    Add ModuleResolution class
    This class allows us to move the module resolution and download process outside the GUI transaction by permitting us to treat both cached and uncached modules equivalently during the lead up to that process.

---
+++ Core/Types/ModuleResolution.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CKAN.Types
+{
+    public class ModuleResolution : IEnumerable<CkanModule>
+    {
+        public List<CkanModule> CachedModules { get; set; }
+        public List<CkanModule> UncachedModules { get; set; }
+
+        public IEnumerable<CkanModule> All => CachedModules.Concat(UncachedModules);
+
+        public int Count => CachedModules.Count + UncachedModules.Count;
+
+        public ModuleResolution(IEnumerable<CkanModule> modules, Func<CkanModule, bool> isCached)
+        {
+            CachedModules = new List<CkanModule>();
+            UncachedModules = new List<CkanModule>();
+
+            foreach (var module in modules)
+            {
+                if (isCached(module))
+                {
+                    CachedModules.Add(module);
+                }
+                else
+                {
+                    UncachedModules.Add(module);
+                }
+            }
+        }
+
+        public IEnumerator<CkanModule> GetEnumerator()
+        {
+            return All.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
