19ec0f128808334c365bf91c2367ee373808eae9
Paul Hebble <pjhebble@gmail.com>
2018-01-09 02:03:35 +0000

    Add back in GUI stuff probably used by the IDE

--- GUI/Main.Designer.cs
+++ GUI/Main.Designer.cs
@@ -124,6 +124,7 @@
             this.splitContainer1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.ModList)).BeginInit();
             this.ModListContextMenuStrip.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
             this.MainTabControl.SuspendLayout();
             this.ManageModsTabPage.SuspendLayout();
             this.ChangesetTabPage.SuspendLayout();
@@ -266,6 +267,8 @@
             // statusStrip1
             //
             this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Location = new System.Drawing.Point(0, 1016);
+            this.statusStrip1.Size = new System.Drawing.Size(1544, 22);
             this.statusStrip1.Padding = new System.Windows.Forms.Padding(2, 0, 21, 0);
             this.statusStrip1.TabIndex = 1;
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[]
@@ -585,12 +588,14 @@
             // StatusLabel
             //
             this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(1050, 29);
             this.StatusLabel.Spring = true;
             this.StatusLabel.Text = "";
             this.StatusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             //
             // StatusProgress
             //
+            this.StatusProgress.Name = "StatusProgress";
             this.StatusProgress.Enabled = true;
             this.StatusProgress.Visible = false;
             this.StatusProgress.Minimum = 0;
@@ -1048,6 +1053,8 @@
             this.splitContainer1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.ModList)).EndInit();
             this.ModListContextMenuStrip.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.MainTabControl.ResumeLayout(false);
             this.ManageModsTabPage.ResumeLayout(false);
             this.ManageModsTabPage.PerformLayout();
