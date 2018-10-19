084e1275e49558d21e39c84ebdcb8ff338e81a6e
Paul Hebble <pjhebble@gmail.com>
2018-10-19 17:51:36 -0500

    Fix toolbar background colors for dark themes

---
+++ GUI/FlatToolStripRenderer.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CKAN
+{
+    /// <summary>
+    /// Custom toolstrip renderer that just fills the background with the BackColor.
+    ///
+    /// The default Mono one uses a gradient between a hardcoded light color and a system color that Mono doesn't load correctly.
+    /// </summary>
+    public class FlatToolStripRenderer : ToolStripProfessionalRenderer
+    {
+
+        protected override void OnRenderToolStripBackground(ToolStripRenderEventArgs e)
+        {
+            // Brushes need to be disposed
+            using (SolidBrush bgBrush = new SolidBrush(e.BackColor))
+            {
+                e.Graphics.FillRectangle(bgBrush, e.AffectedBounds);
+            }
+        }
+
+    }
+}
