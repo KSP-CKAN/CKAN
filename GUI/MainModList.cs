73c2dace2f7661210bd6d12bfae4444431027a75
Paul Hebble <pjhebble@gmail.com>
2019-01-11 00:11:35 +0000

    Set focus to mod list after loading

--- GUI/MainModList.cs
+++ GUI/MainModList.cs
@@ -258,6 +258,7 @@ namespace CKAN
             tabController.HideTab("WaitTabPage");
             tabController.SetTabLock(false);
             Util.Invoke(this, SwitchEnabledState);
+            Util.Invoke(this, () => Main.Instance.ModList.Focus());
         }
 
         public void MarkModForInstall(string identifier, bool uncheck = false)
