1a3b97263fefcd7ad5d09c72b6bc0e4472b76c6f
Paul Hebble <pjhebble@gmail.com>
2019-06-28 17:07:22 +0000

    Fix Netkan localization parser performance

--- Netkan/Transformers/LocalizationsTransformer.cs
+++ Netkan/Transformers/LocalizationsTransformer.cs
@@ -90,7 +90,7 @@ namespace CKAN.NetKAN.Transformers
         private static readonly ILog log = LogManager.GetLogger(typeof(LocalizationsTransformer));
 
         private static readonly Regex localizationRegex = new Regex(
-            @"^\s*Localization\b.*?{(?<contents>.*?([-a-zA-Z]+.*?{.*?}.*?)*?)}",
+            @"^\s*Localization\b\s*{(?<contents>([^{}]*{[^{}]*})+[^{}]*)}",
             RegexOptions.Compiled | RegexOptions.Multiline | RegexOptions.Singleline
         );
         private static readonly Regex localeRegex = new Regex(
