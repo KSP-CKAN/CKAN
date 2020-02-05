08584169637d3ef34d641faa228f0f8692d2f723
Paul Hebble <pjhebble@gmail.com>
2020-02-05 20:55:14 +0000

    Only update provides tab from GUI thread

--- GUI/Main/MainProvides.cs
+++ GUI/Main/MainProvides.cs
@@ -8,13 +8,5 @@ namespace CKAN
         {
             ShowSelectionModInfo(items);
         }
-
-        private CkanModule TooManyModsProvideCore(TooManyModsProvideKraken kraken)
-        {
-            tabController.ShowTab("ChooseProvidedModsTabPage", 3);
-            ChooseProvidedMods.LoadProviders(kraken.requested, kraken.modules, Manager.Cache);
-            tabController.SetTabLock(true);
-            return ChooseProvidedMods.Wait();
-        }
     }
 }
