01b43cc74a5814dec2a69c7ea91557024302c0c1
Paul Hebble <pjhebble@gmail.com>
2020-05-24 13:16:55 -0500

    Netkan warning for archived repos, set bugtracker for GitHub

--- Netkan/Sources/Github/GithubRepo.cs
+++ Netkan/Sources/Github/GithubRepo.cs
@@ -21,15 +21,21 @@ namespace CKAN.NetKAN.Sources.Github
 
         [JsonProperty("license")]
         public GithubLicense License { get; set; }
-        
+
         [JsonProperty("parent")]
         public GithubRepo ParentRepo { get; set; }
-        
+
         [JsonProperty("source")]
         public GithubRepo SourceRepo { get; set; }
-        
+
         [JsonProperty("owner")]
         public GithubUser Owner { get; set; }
+
+        [JsonProperty("has_issues")]
+        public bool HasIssues { get; set; }
+
+        [JsonProperty("archived")]
+        public bool Archived { get; set; }
     }
 
     public class GithubLicense
@@ -37,12 +43,12 @@ namespace CKAN.NetKAN.Sources.Github
         [JsonProperty("spdx_id")]
         public string Id;
     }
-    
+
     public class GithubUser
     {
         [JsonProperty("login")]
         public string Login { get; set; }
-        
+
         [JsonProperty("type")]
         public string Type { get; set; }
     }
