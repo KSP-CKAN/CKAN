38d8f6f421fcb7f3b0d006a488e9c4e7f26ce293
Paul Hebble <pjhebble@gmail.com>
2023-11-09 11:40:03 -0600

    Update copyright dates

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -2,7 +2,7 @@ using System.Reflection;
 
 [assembly: AssemblyProduct("CKAN")]
 [assembly: AssemblyCompany("CKAN Contributors")]
-[assembly: AssemblyCopyright("Copyright © 2014–2022")]
+[assembly: AssemblyCopyright("Copyright © 2014–2023")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
