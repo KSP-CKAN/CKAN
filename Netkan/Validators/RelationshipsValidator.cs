8864b3bb9d5c9d62361e46562ca5371d5e3e25c1
Paul Hebble <pjhebble@gmail.com>
2024-02-10 14:08:08 -0600

    Further improve netkan relationship error message

--- Netkan/Validators/RelationshipsValidator.cs
+++ Netkan/Validators/RelationshipsValidator.cs
@@ -1,5 +1,4 @@
-using System.Linq;
-
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 using CKAN.Versioning;
@@ -20,49 +19,64 @@ namespace CKAN.NetKAN.Validators
                     {
                         throw new Kraken("spec_version v1.2+ required for 'supports'");
                     }
-                    foreach (var rel in json[relName].Children<JObject>())
+                    foreach (var relToken in json[relName].Children())
                     {
-                        if (rel.ContainsKey("any_of"))
+                        switch (relToken)
                         {
-                            if (metadata.SpecVersion < v1p26)
-                            {
-                                throw new Kraken("spec_version v1.26+ required for 'any_of'");
-                            }
-                            foreach (string forbiddenPropertyName in AnyOfRelationshipDescriptor.ForbiddenPropertyNames)
-                            {
-                                if (rel.ContainsKey(forbiddenPropertyName))
+                            case JObject rel:
+                                if (rel.ContainsKey("any_of"))
                                 {
-                                    throw new Kraken($"{forbiddenPropertyName} is not valid in the same relationship as 'any_of'");
+                                    if (metadata.SpecVersion < v1p26)
+                                    {
+                                        throw new Kraken("spec_version v1.26+ required for 'any_of'");
+                                    }
+                                    foreach (string forbiddenPropertyName in AnyOfRelationshipDescriptor.ForbiddenPropertyNames)
+                                    {
+                                        if (rel.ContainsKey(forbiddenPropertyName))
+                                        {
+                                            throw new Kraken($"{forbiddenPropertyName} is not valid in the same relationship as 'any_of'");
+                                        }
+                                    }
+                                    if (rel.ContainsKey("choice_help_text") && metadata.SpecVersion < v1p31)
+                                    {
+                                        throw new Kraken("spec_version v1.31+ required for choice_help_text in same relationship as 'any_of'");
+                                    }
+                                    foreach (var optToken in rel["any_of"].Children())
+                                    {
+                                        switch (optToken)
+                                        {
+                                            case JObject opt:
+                                                string name = (string)opt["name"];
+                                                if (!Identifier.ValidIdentifierPattern.IsMatch(name))
+                                                {
+                                                    throw new Kraken($"{name} in {relName} 'any_of' is not a valid CKAN identifier");
+                                                }
+                                                break;
+                                            default:
+                                                throw new Kraken($"{relName} 'any_of' elements should be {JTokenType.Object}, found {optToken.Type}: {optToken.ToString(Formatting.None)}");
+                                        }
+                                    }
                                 }
-                            }
-                            if (rel.ContainsKey("choice_help_text") && metadata.SpecVersion < v1p31)
-                            {
-                                throw new Kraken("spec_version v1.31+ required for choice_help_text in same relationship as 'any_of'");
-                            }
-                            foreach (JObject opt in rel["any_of"].Cast<JObject>())
-                            {
-                                string name = (string)opt["name"];
-                                if (!Identifier.ValidIdentifierPattern.IsMatch(name))
+                                else
                                 {
-                                    throw new Kraken($"{name} in {relName} 'any_of' is not a valid CKAN identifier");
+                                    string name = (string)rel["name"];
+                                    if (!Identifier.ValidIdentifierPattern.IsMatch(name))
+                                    {
+                                        throw new Kraken($"{name} in {relName} is not a valid CKAN identifier");
+                                    }
+                                    if (rel.ContainsKey("version_min"))
+                                    {
+                                        throw new Kraken($"'version_min' found in relationship, the correct form is 'min_version'");
+                                    }
+                                    if (rel.ContainsKey("version_max"))
+                                    {
+                                        throw new Kraken($"'version_max' found in relationship, the correct form is 'max_version'");
+                                    }
                                 }
-                            }
-                        }
-                        else
-                        {
-                            string name = (string)rel["name"];
-                            if (!Identifier.ValidIdentifierPattern.IsMatch(name))
-                            {
-                                throw new Kraken($"{name} in {relName} is not a valid CKAN identifier");
-                            }
-                            if (rel.ContainsKey("version_min"))
-                            {
-                                throw new Kraken($"'version_min' found in relationship, the correct form is 'min_version'");
-                            }
-                            if (rel.ContainsKey("version_max"))
-                            {
-                                throw new Kraken($"'version_max' found in relationship, the correct form is 'max_version'");
-                            }
+                                break;
+
+                            default:
+                                throw new Kraken($"{relName} elements should be {JTokenType.Object}, found {relToken.Type}: {relToken.ToString(Formatting.None)}");
                         }
                     }
                 }
