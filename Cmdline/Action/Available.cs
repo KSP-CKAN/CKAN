7547df3663c2e5023c76cbb08c9548871bb4a46e
Paul Hebble <pjhebble@gmail.com>
2024-11-05 15:05:08 -0600

    Glob match option for ckan available

--- Cmdline/Action/Available.cs
+++ Cmdline/Action/Available.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 using CommandLine;
 
@@ -17,10 +19,24 @@ namespace CKAN.CmdLine
             AvailableOptions opts     = (AvailableOptions)raw_options;
             IRegistryQuerier registry = RegistryManager.Instance(instance, repoData).registry;
 
-            var compatible = registry
+            IEnumerable<CkanModule> compatible = registry
                 .CompatibleModules(instance.VersionCriteria())
                 .Where(m => !m.IsDLC)
                 .OrderBy(m => m.identifier);
+
+            if (opts.globs != null)
+            {
+                var regexes = opts.globs
+                                  .Select(str => new Regex("^"
+                                                           + str.Replace("?", ".")
+                                                                .Replace("*", ".*")
+                                                           + "$",
+                                                           RegexOptions.Compiled
+                                                           | RegexOptions.IgnoreCase))
+                                  .ToArray();
+                compatible = compatible.Where(m => regexes.Any(re => re.IsMatch(m.identifier)
+                                                                     || re.IsMatch(m.name)));
+            }
 
             user.RaiseMessage(Properties.Resources.AvailableHeader,
                               instance.game.ShortName,
@@ -31,14 +47,18 @@ namespace CKAN.CmdLine
             {
                 foreach (CkanModule module in compatible)
                 {
-                    user.RaiseMessage("* {0} ({1}) - {2} - {3}", module.identifier, module.version, module.name, module.@abstract);
+                    user.RaiseMessage("* {0} ({1}) - {2} - {3}",
+                                      module.identifier, module.version,
+                                      module.name, module.@abstract);
                 }
             }
             else
             {
                 foreach (CkanModule module in compatible)
                 {
-                    user.RaiseMessage("* {0} ({1}) - {2}", module.identifier, module.version, module.name);
+                    user.RaiseMessage("* {0} ({1}) - {2}",
+                                      module.identifier, module.version,
+                                      module.name);
                 }
             }
 
@@ -53,6 +73,9 @@ namespace CKAN.CmdLine
     {
         [Option("detail", HelpText = "Show short description of each module")]
         public bool detail { get; set; }
+
+        [ValueList(typeof(List<string>))]
+        public List<string>? globs { get; set; }
     }
 
 }
