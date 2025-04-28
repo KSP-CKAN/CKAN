59f0131b6e01bf707936ae138a2cfd22d1d5a3ad
Paul Hebble <pjhebble@gmail.com>
2025-04-27 23:31:31 -0500

    Remove unused using

--- Core/IO/ModuleInstaller.cs
+++ Core/IO/ModuleInstaller.cs
@@ -2,7 +2,6 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
 using System.Threading;
 
 using ICSharpCode.SharpZipLib.Core;
