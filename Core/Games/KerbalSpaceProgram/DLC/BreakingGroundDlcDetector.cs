a999b6fba503f28426aaaad4d0c2a523a3f3ca67
Paul Hebble <pjhebble@gmail.com>
2025-04-27 23:30:44 -0500

    Fix name of DLC in comment

--- Core/Games/KerbalSpaceProgram/DLC/BreakingGroundDlcDetector.cs
+++ Core/Games/KerbalSpaceProgram/DLC/BreakingGroundDlcDetector.cs
@@ -1,7 +1,7 @@
 namespace CKAN.Games.KerbalSpaceProgram.DLC
 {
     /// <summary>
-    /// Represents an object that can detect the presence of the official Making History DLC in a KSP installation.
+    /// Represents an object that can detect the presence of the official Breaking Ground DLC in a KSP installation.
     /// </summary>
     public sealed class BreakingGroundDlcDetector : StandardDlcDetectorBase
     {
