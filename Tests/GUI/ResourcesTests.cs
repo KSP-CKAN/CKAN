235150f7f2bc7704d2d85dec6445a4b8f859a37e
Paul Hebble <pjhebble@gmail.com>
2025-04-27 23:34:34 -0500

    Get forms and controls dynamically for resource tests

--- Tests/GUI/ResourcesTests.cs
+++ Tests/GUI/ResourcesTests.cs
@@ -4,6 +4,9 @@ using System.Resources;
 using System.ComponentModel;
 using System.Globalization;
 using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Windows.Forms;
 
 using NUnit.Framework;
 
@@ -51,75 +54,55 @@ namespace Tests.GUI
         ///
         /// This test covers the controls and dialogs.
         /// </summary>
-        [Test,
-            // Controls
-            TestCase(typeof(CKAN.GUI.Metadata)),
-            TestCase(typeof(CKAN.GUI.Relationships)),
-            TestCase(typeof(CKAN.GUI.Contents)),
-            TestCase(typeof(CKAN.GUI.Versions)),
-            TestCase(typeof(CKAN.GUI.Changeset)),
-            TestCase(typeof(CKAN.GUI.ChooseProvidedMods)),
-            TestCase(typeof(CKAN.GUI.ChooseRecommendedMods)),
-            TestCase(typeof(CKAN.GUI.DeleteDirectories)),
-            TestCase(typeof(CKAN.GUI.EditModpack)),
-            TestCase(typeof(CKAN.GUI.EditModSearchDetails)),
-            TestCase(typeof(CKAN.GUI.EditModSearch)),
-            TestCase(typeof(CKAN.GUI.InstallationHistory)),
-            TestCase(typeof(CKAN.GUI.ManageMods)),
-            TestCase(typeof(CKAN.GUI.ModInfo)),
-            TestCase(typeof(CKAN.GUI.PlayTime)),
-            TestCase(typeof(CKAN.GUI.UnmanagedFiles)),
-            TestCase(typeof(CKAN.GUI.Wait)),
-
-            // Dialogs
-            TestCase(typeof(CKAN.GUI.Main)),
-            TestCase(typeof(CKAN.GUI.AboutDialog)),
-            TestCase(typeof(CKAN.GUI.AskUserForAutoUpdatesDialog)),
-            TestCase(typeof(CKAN.GUI.CloneGameInstanceDialog)),
-            TestCase(typeof(CKAN.GUI.CompatibleGameVersionsDialog)),
-            TestCase(typeof(CKAN.GUI.DownloadsFailedDialog)),
-            TestCase(typeof(CKAN.GUI.EditLabelsDialog)),
-            TestCase(typeof(CKAN.GUI.ErrorDialog)),
-            TestCase(typeof(CKAN.GUI.GameCommandLineOptionsDialog)),
-            TestCase(typeof(CKAN.GUI.InstallFiltersDialog)),
-            TestCase(typeof(CKAN.GUI.ManageGameInstancesDialog)),
-            TestCase(typeof(CKAN.GUI.NewRepoDialog)),
-            TestCase(typeof(CKAN.GUI.NewUpdateDialog)),
-            TestCase(typeof(CKAN.GUI.PluginsDialog)),
-            TestCase(typeof(CKAN.GUI.PreferredHostsDialog)),
-            TestCase(typeof(CKAN.GUI.RenameInstanceDialog)),
-            TestCase(typeof(CKAN.GUI.SelectionDialog)),
-            TestCase(typeof(CKAN.GUI.SettingsDialog)),
-            TestCase(typeof(CKAN.GUI.YesNoDialog)),
-        ]
+        [TestCaseSource(nameof(DialogsAndControls))]
         public void ControlOrDialog_AllLocales_LanguageNotSetAndAllStrings(Type t)
         {
-            // Arrange
-            ComponentResourceManager resources = new CKAN.GUI.SingleAssemblyComponentResourceManager(t);
-
-            // Act/Assert
-            Assert.Multiple(() =>
+            try
             {
-                foreach (CultureInfo resourceCulture in cultures)
-                {
-                    Assert.IsNull(resources.GetObject("$this.Language", resourceCulture));
+                // Arrange
+                ComponentResourceManager resources = new CKAN.GUI.SingleAssemblyComponentResourceManager(t);
 
-                    var resSet = resources.GetResourceSet(resourceCulture, false, false);
-                    if (resSet != null)
+                // Act/Assert
+                Assert.Multiple(() =>
+                {
+                    foreach (CultureInfo resourceCulture in cultures)
                     {
-                        foreach (DictionaryEntry entry in resSet)
+                        Assert.IsNull(resources.GetObject("$this.Language", resourceCulture));
+
+                        var resSet = resources.GetResourceSet(resourceCulture, false, false);
+                        if (resSet != null)
                         {
-                            Assert.IsInstanceOf<string>(entry.Value,
-                                $"Resource '{t.Name} {entry.Key}' in locale '{resourceCulture.Name}' is not a string");
+                            foreach (DictionaryEntry entry in resSet)
+                            {
+                                Assert.IsInstanceOf<string>(entry.Value,
+                                    $"Resource '{t.Name} {entry.Key}' in locale '{resourceCulture.Name}' is not a string");
+                            }
                         }
                     }
-                }
-            });
+                });
+            }
+            catch (MissingManifestResourceException)
+            {
+                // A few controls don't have resources, and that's OK
+            }
         }
 
+        // The types to test
+        private static IEnumerable<Type> DialogsAndControls =>
+            Assembly.GetAssembly(typeof(CKAN.GUI.Main))
+                    .GetTypes()
+                    .Where(t => !t.IsAbstract && baseTypesToCheck.Any(t.IsSubclassOf));
+
+        private static readonly Type[] baseTypesToCheck = new Type[]
+        {
+            typeof(UserControl),
+            typeof(Form),
+        };
+
         // The cultures to test
-        private static readonly CultureInfo[] cultures = CKAN.Utilities.AvailableLanguages
-            .Select(l => new CultureInfo(l))
-            .ToArray();
+        private static readonly CultureInfo[] cultures =
+            CKAN.Utilities.AvailableLanguages
+                          .Select(l => new CultureInfo(l))
+                          .ToArray();
     }
 }
