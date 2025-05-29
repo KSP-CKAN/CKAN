2f4b3ca07105714101cb25d1dd106a86803bed03
Paul Hebble <pjhebble@gmail.com>
2025-05-29 08:32:30 -0500

    Use relative luminance to determine module label ForeColors

--- Tests/GUI/UtilTests.cs
+++ Tests/GUI/UtilTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Drawing;
 
@@ -15,13 +16,14 @@ namespace Tests.GUI
          TestCase("OrangeRed"), TestCase("LightCoral"),
          TestCase("Yellow"), TestCase("Gold"), TestCase("Khaki"),
          TestCase("PaleGreen"), TestCase("Lime"),
+         TestCase("#408080" /* Paradiso */),
          TestCase("SkyBlue"), TestCase("Aqua"), TestCase("LightBlue"), TestCase("DeepSkyBlue"),
          TestCase("Violet"),
          TestCase("Gray"),
         ]
         public void ForeColorForBackColor_LightBackColor_BlackForeColor(string colorName)
         {
-            var c = Color.FromName(colorName);
+            var c = ColorFromNameOrRGB(colorName);
             Assert.AreEqual(Color.Black, c.ForeColorForBackColor(),
                             $"Foreground color for {c.Name} (brightness {c.GetBrightness()}) should be Black");
         }
@@ -31,14 +33,25 @@ namespace Tests.GUI
          TestCase("Green"),
          TestCase("DarkBlue"), TestCase("MidnightBlue"), TestCase("Navy"), TestCase("DarkSlateBlue"), TestCase("MediumBlue"),
          TestCase("Indigo"), TestCase("Purple"), TestCase("DarkViolet"),
+         TestCase("#8000FF" /* ElectricViolet */),
          TestCase("Brown"), TestCase("Sienna"),
          TestCase("DimGray"),
         ]
         public void ForeColorForBackColor_DarkBackColor_WhiteForeColor(string colorName)
         {
-            var c = Color.FromName(colorName);
+            var c = ColorFromNameOrRGB(colorName);
             Assert.AreEqual(Color.White, c.ForeColorForBackColor(),
                             $"Foreground color for {c.Name} (brightness {c.GetBrightness()}) should be White");
         }
+
+        private static Color ColorFromNameOrRGB(string nameOrRGB)
+            => nameOrRGB.StartsWith("#") ? Color.FromArgb(StringToColorChannel(nameOrRGB, 1),
+                                                          StringToColorChannel(nameOrRGB, 3),
+                                                          StringToColorChannel(nameOrRGB, 5))
+                                         : Color.FromName(nameOrRGB);
+
+        private static int StringToColorChannel(string rgb, int start)
+            => Convert.ToInt32(rgb.Substring(start, 2), 16);
+
     }
 }
