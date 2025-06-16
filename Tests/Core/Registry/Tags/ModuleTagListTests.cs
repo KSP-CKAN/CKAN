27fb414cebc8a2251addb194e0a5d2a065d91e30
Paul Hebble <pjhebble@gmail.com>
2025-06-16 15:47:41 -0500

    Test ModuleTagList

---
+++ Tests/Core/Registry/Tags/ModuleTagListTests.cs
@@ -0,0 +1,41 @@
+using System.IO;
+
+using NUnit.Framework;
+
+using CKAN;
+using Tests.Data;
+
+namespace Tests.Core
+{
+    [TestFixture]
+    public sealed class ModuleTagListTests
+    {
+        [Test]
+        public void Load_FromSave_Works()
+        {
+            // Arrange
+            using (var dir = new TemporaryDirectory())
+            {
+                var path = Path.Combine(dir.Path.FullName, "mtl.json");
+                var orig = new ModuleTagList();
+
+                // Act
+                orig.HiddenTags.Add("library");
+                orig.HiddenTags.Add("flags");
+                orig.HiddenTags.Add("agency");
+                orig.Save(path);
+
+                var loaded = ModuleTagList.Load(path)!;
+
+                // Assert
+                CollectionAssert.AreEquivalent(new string[]
+                                               {
+                                                   "library",
+                                                   "flags",
+                                                   "agency",
+                                               },
+                                               loaded.HiddenTags);
+            }
+        }
+    }
+}
