ae5be550edace6653291c09442a79cecdd524641
Paul Hebble <pjhebble@gmail.com>
2025-06-16 10:51:43 -0500

    Test SuppressedCompatWarningIdentifiers

---
+++ Tests/Core/SuppressedCompatWarningIdentifiersTests.cs
@@ -0,0 +1,44 @@
+using System.IO;
+using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using CKAN;
+using CKAN.Versioning;
+using Tests.Data;
+
+namespace Tests.Core
+{
+    [TestFixture]
+    public sealed class SuppressedCompatWarningIdentifiersTests
+    {
+        [Test]
+        public void LoadFrom_GeneratedBySaveTo_Works()
+        {
+            // Arrange
+            using (var dir = new TemporaryDirectory())
+            {
+                var gameVer     = new GameVersion(1, 12, 5);
+                var identifiers = new HashSet<string>
+                {
+                    "BadVersionFileMod",
+                    "NoVersionFileMod",
+                    "OverlyCautiousMod",
+                };
+                var suppressed = new SuppressedCompatWarningIdentifiers()
+                {
+                    GameVersionWhenWritten = gameVer,
+                    Identifiers            = identifiers,
+                };
+                var filename = Path.Combine(dir.Path.FullName, "suppressed.json");
+
+                // Act
+                suppressed.SaveTo(filename);
+                var deserialized = SuppressedCompatWarningIdentifiers.LoadFrom(gameVer, filename);
+
+                // Assert
+                CollectionAssert.AreEquivalent(identifiers, deserialized.Identifiers);
+            }
+        }
+    }
+}
