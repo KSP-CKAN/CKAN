f428b2882f92d5996916bd8614e818db2b5dd247
Paul Hebble <pjhebble@gmail.com>
2025-07-06 19:13:15 -0500

    Refactor Net.Download exception handling

--- Tests/Core/Net/Net.cs
+++ Tests/Core/Net/Net.cs
@@ -1,55 +1,64 @@
+using System;
 using System.IO;
-using System.Net;
 
 using NUnit.Framework;
 
 namespace Tests.Core.Net
 {
+    using Net = CKAN.Net;
+
     [TestFixture]
     [Category("Online")]
     public class NetTests
     {
         // TODO: Test certificate errors. How?
         // URL we expect to always be up.
-        private const string KnownURL = "http://example.com/";
+        private static readonly Uri KnownURL = new Uri("http://example.com/");
 
-        [Test]
-        public void DownloadThrowsOnInvalidURL()
+        [TestCase("cheese sandwich")]
+        public void Download_InvalidURL_Throws(string url)
         {
             // Download should throw an exception on an invalid URL.
-            Assert.Throws<WebException>(
-                delegate
-                {
-                    CKAN.Net.Download("cheese sandwich");
-                });
+            Assert.Throws<UriFormatException>(() =>
+            {
+                Net.Download(new Uri(url), out _);
+            });
         }
 
-        [Test]
-        public void DownloadReturnsSavefileNameAndSavefileExists()
+        [TestCase("example.txt")]
+        public void Download_WithFilename_ReturnsSavefileNameAndSavefileExists(string savefile)
         {
             // Three-argument test, should save to the file we supply
-            string savefile = "example.txt";
-            string downloaded = CKAN.Net.Download(KnownURL, null, savefile);
+            string downloaded = Net.Download(KnownURL, out _, null, savefile);
             Assert.AreEqual(savefile, downloaded);
             Assert.That(File.Exists(savefile));
             File.Delete(savefile);
         }
 
         [Test]
-        public void SingleArgumentDownloadSavesToTemporaryFile()
+        public void Download_NoFilename_SavesToTemporaryFile()
         {
-            string downloaded = CKAN.Net.Download(KnownURL);
+            string downloaded = Net.Download(KnownURL, out _);
             Assert.That(File.Exists(downloaded));
             File.Delete(downloaded);
         }
 
-        [Test]
+        [TestCase("https://spacedock.info/mod/132/Contract%20Reward%20Modifier/download/2.1")]
         [Category("FlakyNetwork")]
-        public void SpaceDockSSL()
+        public void Download_SpaceDock_Works(string url)
         {
-            string downloaded = CKAN.Net.Download("https://spacedock.info/mod/132/Contract%20Reward%20Modifier/download/2.1");
+            // Arrange
+            var uri = new Uri(url);
+
+            // Act
+            string downloaded = Net.Download(uri, out _);
+
+            // Assert
             Assert.That(File.Exists(downloaded), "File not downloaded");
+
+            // Teardown
             File.Delete(downloaded);
         }
+
     }
 }
