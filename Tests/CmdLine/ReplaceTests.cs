edf1c4a875c93b7414c093261cc577acd212cdb4
Paul Hebble <pjhebble@gmail.com>
2025-09-15 10:08:15 -0500

    Test replace all

--- Tests/CmdLine/ReplaceTests.cs
+++ Tests/CmdLine/ReplaceTests.cs
@@ -14,39 +14,38 @@ namespace Tests.CmdLine
     [TestFixture]
     public class ReplaceTests
     {
-        [Test,
-            TestCase(new string[]
-                     {
-                         @"{
-                             ""spec_version"": 1,
-                             ""identifier"":   ""ReplaceableMod"",
-                             ""version"":      ""1.0"",
-                             ""download"":     ""https://github.com/"",
-                             ""install"": [
-                                 {
-                                     ""find"": ""DogeCoinFlag"",
-                                     ""install_to"": ""GameData""
-                                 }
-                             ],
-                             ""replaced_by"": { ""name"": ""ReplacementMod"" }
-                         }",
-                         @"{
-                             ""spec_version"": 1,
-                             ""identifier"":   ""ReplacementMod"",
-                             ""version"":      ""1.0"",
-                             ""download"":     ""https://github.com/"",
-                             ""install"": [
-                                 {
-                                     ""find"": ""DogeCoinFlag"",
-                                     ""install_to"": ""GameData""
-                                 }
-                             ]
-                         }",
-                     },
-                     "ReplaceableMod",
-                     "1.0",
-                     "ReplacementMod",
-                     "1.0"),
+        [TestCase(new string[]
+                  {
+                      @"{
+                          ""spec_version"": 1,
+                          ""identifier"":   ""ReplaceableMod"",
+                          ""version"":      ""1.0"",
+                          ""download"":     ""https://github.com/"",
+                          ""install"": [
+                              {
+                                  ""find"": ""DogeCoinFlag"",
+                                  ""install_to"": ""GameData""
+                              }
+                          ],
+                          ""replaced_by"": { ""name"": ""ReplacementMod"" }
+                      }",
+                      @"{
+                          ""spec_version"": 1,
+                          ""identifier"":   ""ReplacementMod"",
+                          ""version"":      ""1.0"",
+                          ""download"":     ""https://github.com/"",
+                          ""install"": [
+                              {
+                                  ""find"": ""DogeCoinFlag"",
+                                  ""install_to"": ""GameData""
+                              }
+                          ]
+                      }",
+                  },
+                  "ReplaceableMod",
+                  "1.0",
+                  "ReplacementMod",
+                  "1.0"),
         ]
         public void RunCommand_ReplaceableMod_Works(string[] modules,
                                                     string   ident1,
@@ -70,6 +69,80 @@ namespace Tests.CmdLine
                 var opts = new ReplaceOptions()
                 {
                     modules = new List<string> { ident1 },
+                };
+
+                // Act
+                regMgr.registry.RegisterModule(module1, new string[] { }, inst.KSP, false);
+                manager.Cache?.Store(module2, TestData.DogeCoinFlagZip(), null);
+                ICommand cmd = new Replace(manager, repoData.Manager, user);
+                cmd.RunCommand(inst.KSP, opts);
+
+                // Assert
+                Assert.Multiple(() =>
+                {
+                    CollectionAssert.AreEqual(Enumerable.Empty<string>(),
+                                              user.RaisedErrors);
+                    CollectionAssert.AreEqual(new CkanModule[] { module2 },
+                                              regMgr.registry.InstalledModules.Select(m => m.Module));
+                });
+            }
+        }
+
+        [TestCase(new string[]
+                  {
+                      @"{
+                          ""spec_version"": 1,
+                          ""identifier"":   ""ReplaceableMod"",
+                          ""version"":      ""1.0"",
+                          ""download"":     ""https://github.com/"",
+                          ""install"": [
+                              {
+                                  ""find"": ""DogeCoinFlag"",
+                                  ""install_to"": ""GameData""
+                              }
+                          ],
+                          ""replaced_by"": { ""name"": ""ReplacementMod"" }
+                      }",
+                      @"{
+                          ""spec_version"": 1,
+                          ""identifier"":   ""ReplacementMod"",
+                          ""version"":      ""1.0"",
+                          ""download"":     ""https://github.com/"",
+                          ""install"": [
+                              {
+                                  ""find"": ""DogeCoinFlag"",
+                                  ""install_to"": ""GameData""
+                              }
+                          ]
+                      }",
+                  },
+                  "ReplaceableMod",
+                  "1.0",
+                  "ReplacementMod",
+                  "1.0"),
+        ]
+        public void RunCommand_All_Works(string[] modules,
+                                                    string   ident1,
+                                                    string   version1,
+                                                    string   ident2,
+                                                    string   version2)
+        {
+            // Arrange
+            var user = new CapturingUser(false, q => true, (msg, objs) => 0);
+            using (var inst     = new DisposableKSP())
+            using (var repo     = new TemporaryRepository(modules))
+            using (var repoData = new TemporaryRepositoryData(user, repo.repo))
+            using (var config   = new FakeConfiguration(inst.KSP, inst.KSP.Name))
+            using (var manager  = new GameInstanceManager(user, config))
+            using (var regMgr   = RegistryManager.Instance(inst.KSP, repoData.Manager,
+                                                           new Repository[] { repo.repo }))
+            {
+                manager.SetCurrentInstance(inst.KSP);
+                var module1 = regMgr.registry.GetModuleByVersion(ident1, version1)!;
+                var module2 = regMgr.registry.GetModuleByVersion(ident2, version2)!;
+                var opts = new ReplaceOptions()
+                {
+                    replace_all = true,
                 };
 
                 // Act
