d06ef5fa7e14c8bec637daaeeaf00a27f400677e
Paul Hebble <pjhebble@gmail.com>
2025-09-23 09:43:43 -0500

    Test ckan install with virtual provider choice

--- Tests/CmdLine/InstallTests.cs
+++ Tests/CmdLine/InstallTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -153,6 +154,98 @@ namespace Tests.CmdLine
                                               "Usage: ckan install [options] module [module2 ...]"
                                           },
                                           user.RaisedErrors);
+            }
+        }
+
+        [TestCase(-1, new string[] {}),
+         TestCase( 0, new string[] { "InstallingMod", "Provider1" }),
+         TestCase( 1, new string[] { "InstallingMod", "Provider2" }),
+        ]
+        public void RunCommand_VirtualDepends_PromptsUser(int answer, string[] expectedInstalled)
+        {
+            // Arrange
+            var     asked    = false;
+            string? question = null;
+            var user = new CapturingUser(false, q => true,
+                                         (msg, objs) =>
+                                         {
+                                             asked    = true;
+                                             question = msg;
+                                             return answer;
+                                         });
+            using (var inst     = new DisposableKSP())
+            using (var repo     = new TemporaryRepository(Core.Relationships.RelationshipResolverTests.MergeWithDefaults(
+                                      @"{
+                                          ""identifier"": ""InstallingMod"",
+                                          ""depends"":    [ { ""name"": ""Virtual"" } ],
+                                          ""install"":    [
+                                              {
+                                                  ""find"":       ""DogeCoinFlag"",
+                                                  ""install_to"": ""GameData"",
+                                                  ""as"":         ""InstallingMod""
+                                              }
+                                          ]
+                                      }",
+                                      @"{
+                                          ""identifier"": ""Provider1"",
+                                          ""provides"":   [ ""Virtual"" ],
+                                          ""install"":    [
+                                              {
+                                                  ""find"":       ""DogeCoinFlag"",
+                                                  ""install_to"": ""GameData"",
+                                                  ""as"":         ""Provider1""
+                                              }
+                                          ]
+                                      }",
+                                      @"{
+                                          ""identifier"": ""Provider2"",
+                                          ""provides"":   [ ""Virtual"" ],
+                                          ""install"":    [
+                                              {
+                                                  ""find"":       ""DogeCoinFlag"",
+                                                  ""install_to"": ""GameData"",
+                                                  ""as"":         ""Provider2""
+                                              }
+                                          ]
+                                      }")
+                                      .ToArray()))
+            using (var repoData = new TemporaryRepositoryData(user, repo.repo))
+            using (var config   = new FakeConfiguration(inst.KSP, inst.KSP.Name))
+            using (var manager  = new GameInstanceManager(user, config))
+            using (var regMgr   = RegistryManager.Instance(inst.KSP, repoData.Manager,
+                                                           new Repository[] { repo.repo }))
+            {
+                manager.SetCurrentInstance(inst.KSP);
+                var opts = new InstallOptions()
+                {
+                    modules = new List<string> { "InstallingMod" },
+                };
+                ICommand cmd = new Install(manager, repoData.Manager, user);
+
+                // Act
+                foreach (var module in regMgr.registry.CompatibleModules(inst.KSP.StabilityToleranceConfig,
+                                                                         inst.KSP.VersionCriteria()))
+                {
+                    manager.Cache!.Store(module, TestData.DogeCoinFlagZip(), null);
+                }
+                cmd.RunCommand(inst.KSP, opts);
+
+                // Assert
+                Assert.Multiple(() =>
+                {
+                    CollectionAssert.IsEmpty(user.RaisedErrors);
+                    Assert.IsTrue(asked);
+                    CollectionAssert.AreEqual(new string[]
+                                              {
+                                                  "A mod or modpack requires Virtual, which can be provided by multiple mods.",
+                                                  "",
+                                                  "Which Virtual provider would you like to install?",
+                                              },
+                                              question?.Split(new string[] { "\r\n" },
+                                                              StringSplitOptions.None));
+                    CollectionAssert.AreEquivalent(expectedInstalled,
+                                                   regMgr.registry.InstalledModules.Select(im => im.identifier));
+                });
             }
         }
     }
