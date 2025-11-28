28c29d7ba572baf69bc57edf9f8762330656e858
Dan <54538634+dandoesgithub@users.noreply.github.com>
2025-11-28 19:04:36 +0100

    Update KerbalSpaceProgram2.cs
    add redux installer

--- Core/Games/KerbalSpaceProgram2.cs
+++ Core/Games/KerbalSpaceProgram2.cs
@@ -74,6 +74,7 @@ namespace CKAN.Games.KerbalSpaceProgram2
 
         public string[] CreateableDirs => new string[]
         {
+            "redux_updater"
             "mods",
             "GameData",
             "GameData/Mods",
@@ -257,6 +258,7 @@ namespace CKAN.Games.KerbalSpaceProgram2
         // (PrimaryModDirectoryRelative is allowed implicitly)
         private readonly Dictionary<string, string> allowedFolders = new Dictionary<string, string>
         {
+            { "redux_updater",         "redux_updater"        }
             { "mods",         "mods"         },
             { "BepInEx",         "BepInEx"         },
             { "BepInEx/plugins", "BepInEx/plugins" },
