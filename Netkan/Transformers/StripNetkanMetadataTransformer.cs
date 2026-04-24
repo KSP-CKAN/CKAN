dc5238da78564e7ff1beafdd8ec82b3a47ea32f8
Paul Hebble <pjhebble@gmail.com>
2026-04-24 14:38:27 -0500

    Share $vref among all netkan sections

--- Netkan/Transformers/StripNetkanMetadataTransformer.cs
+++ Netkan/Transformers/StripNetkanMetadataTransformer.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using CKAN.NetKAN.Model;
+
 using log4net;
 using Newtonsoft.Json.Linq;
+
+using CKAN.NetKAN.Model;
+using CKAN.NetKAN.Extensions;
 
 namespace CKAN.NetKAN.Transformers
 {
@@ -23,46 +25,14 @@ namespace CKAN.NetKAN.Transformers
             Log.Debug("Executing strip Netkan metadata transformation");
             Log.DebugFormat("Input metadata:{0}{1}", Environment.NewLine, metadata.AllJson);
 
-            Strip(json);
+            json.StripProperties(IsNetkanProperty);
 
             Log.DebugFormat("Transformed metadata:{0}{1}", Environment.NewLine, json);
 
             yield return new Metadata(json);
         }
 
-        private static bool IsNetkanProperty(string propertyName)
-            => propertyName.StartsWith("x_netkan") || propertyName is "$kref" or "$vref";
-
-        public static JObject Strip(JObject json)
-        {
-            var propertiesToRemove = new List<string>();
-            foreach (var property in json.Properties())
-            {
-                if (IsNetkanProperty(property.Name))
-                {
-                    propertiesToRemove.Add(property.Name);
-                }
-                else
-                {
-                    switch (property.Value)
-                    {
-                        case JObject jobj:
-                            Strip(jobj);
-                            break;
-                        case JArray jarr:
-                            foreach (var element in jarr.OfType<JObject>())
-                            {
-                                Strip(element);
-                            }
-                            break;
-                    }
-                }
-            }
-            foreach (var propertyName in propertiesToRemove)
-            {
-                json.Remove(propertyName);
-            }
-            return json;
-        }
+        private static bool IsNetkanProperty(JProperty prop)
+            => prop.Name.StartsWith("x_netkan") || prop.Name is "$kref" or "$vref";
     }
 }
