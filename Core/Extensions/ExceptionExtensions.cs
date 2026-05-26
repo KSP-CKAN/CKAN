637a5b6b284a85d8ed609380fb427077b9dcc299
Paul Hebble <pjhebble@gmail.com>
2026-05-26 17:23:04 -0500

    Exclude RethrowInner from stack traces

--- Core/Extensions/ExceptionExtensions.cs
+++ Core/Extensions/ExceptionExtensions.cs
@@ -2,11 +2,15 @@ using System;
 using System.Linq;
 using System.Collections.ObjectModel;
 using System.Runtime.ExceptionServices;
+using System.Diagnostics;
 
 namespace CKAN.Extensions
 {
     public static class ExceptionExtensions
     {
+        #if NET6_0_OR_GREATER
+        [StackTraceHidden]
+        #endif
         public static void RethrowInner(this AggregateException agExc)
         {
             var inner = agExc switch
