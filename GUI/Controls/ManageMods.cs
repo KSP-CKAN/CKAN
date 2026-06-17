32904db002971cce4c8433dad71248b99752d014
Paul Hebble <pjhebble@gmail.com>
2026-06-17 10:21:38 -0500

    Only set style to null for rows with uninstalling style

--- GUI/Controls/ManageMods.cs
+++ GUI/Controls/ManageMods.cs
@@ -218,7 +218,7 @@ namespace CKAN.GUI
                             // Set strikeout font for rows being uninstalled
                             row.DefaultCellStyle = uninstallingStyle;
                         }
-                        else if (row.DefaultCellStyle != null)
+                        else if (row.DefaultCellStyle == uninstallingStyle)
                         {
                             // Clear strikeout font for rows not being uninstalled
                             row.DefaultCellStyle = null;
